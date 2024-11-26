b162b2316f33d47502b1b5786c69db4ce25ed918
erfaw <146765011+erfaw@users.noreply.github.com>
2024-11-26 22:40:10 +0330

    part1 - my solution

--- Day008-erf/t03 - Caesar Cipher Part 1.py
+++ Day008-erf/t03 - Caesar Cipher Part 1.py
@@ -1,17 +1,26 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+from clear_sc import clear_screen
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 # direction = input("Type 'encode' to encrypt, type 'decode' to decrypt")
-# text = input("Type your message:\n").lower()
-# shift = int(input("Type the shift number:\n"))
 
-#TODO-1
-def encrypt(original_text, shift_amount):
-  encrypt_message = ''
-  for x in original_text:
-    real_index = alphabet.index(x)
-    encrypt_index = real_index + shift_amount
-    encrypt_letter = alphabet[encrypt_index]
-    encrypt_message += encrypt_letter
-  return encrypt_message
 
-print(encrypt('hello', 1))
+def main():
+  clear_screen()
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number(1-25):\n"))
+  def encrypt(original_text, shift_amount):
+    encrypt_message = ''
+    for x in original_text:
+      real_index = alphabet.index(x)
+      encrypt_index = real_index + shift_amount
+      encrypt_letter = alphabet[encrypt_index]
+      encrypt_message += encrypt_letter
+    print(encrypt_message)
+
+  encrypt(text, shift)
+
+
+while True:
+  main()
+  if input("continue?(y/n)").lower() !='y':
+    break

3cc56dc9d398dc87ddbb926e37b6adb85a8e39a4
erfaw <146765011+erfaw@users.noreply.github.com>
2024-11-28 20:00:36 +0330

    caesar cipher - part2 todos done bedone didan solution

--- Day008-erf/t03 - Caesar Cipher Part 1.py
+++ Day008-erf/t03 - Caesar Cipher Part 1.py
@@ -1,26 +1,44 @@
 from clear_sc import clear_screen
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt")
 
 
 def main():
-  clear_screen()
-  text = input("Type your message:\n").lower()
-  shift = int(input("Type the shift number(1-25):\n"))
-  def encrypt(original_text, shift_amount):
-    encrypt_message = ''
-    for x in original_text:
-      real_index = alphabet.index(x)
-      encrypt_index = real_index + shift_amount
-      encrypt_letter = alphabet[encrypt_index]
-      encrypt_message += encrypt_letter
-    print(encrypt_message)
+    clear_screen()
 
-  encrypt(text, shift)
 
+    def caesar(direction, original_text, shift_amount):
+        def encrypt():
+            encrypt_message = ''
+            for x in original_text:
+                encrypt_index = alphabet.index(x) + shift_amount
+                encrypt_message += alphabet[encrypt_index]
+            print(encrypt_message)
+
+
+        def decrypt():
+            decrypt_message = ''
+            for x in original_text:
+                encrypt_index = alphabet.index(x) - shift_amount
+                decrypt_message += alphabet[encrypt_index]
+            print(decrypt_message)
+
+
+        if direction == 'encode':
+            encrypt()
+        elif direction == 'decode':
+            decrypt()
+        else:
+            input('pick again , wrong choice')
+        # encrypt(text, shift) # erfan -2-> gthcp
+        # decrypt(text, shift)
+    
+    direction = input("Type 'encode' to encrypt,\nType 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number(1-25):\n"))
+    caesar(direction, text, shift)
 
 while True:
-  main()
-  if input("continue?(y/n)").lower() !='y':
-    break
+    main()
+    if input("continue?(y/n)").lower() !='y':
+        break

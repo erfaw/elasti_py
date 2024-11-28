8803907b6a9b2399092283a05b2f50d77d23f688
erfaw <146765011+erfaw@users.noreply.github.com>
2024-11-28 20:25:22 +0330

    part 2 eslah shd ba ravesh course

--- Day008-erf/t03 - Caesar Cipher Part 1.py
+++ Day008-erf/t03 - Caesar Cipher Part 1.py
@@ -1,5 +1,5 @@
 from clear_sc import clear_screen
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
 
@@ -7,35 +7,50 @@ def main():
     clear_screen()
 
 
+    # def caesar(direction, original_text, shift_amount):
+    #     def encrypt():
+    #         encrypt_message = ''
+    #         for x in original_text:
+    #             encrypt_index = alphabet.index(x) + shift_amount
+    #             encrypt_index %= len(alphabet)
+    #             encrypt_message += alphabet[encrypt_index]
+    #         print(encrypt_message)
+
+
+    #     def decrypt():
+    #         decrypt_message = ''
+    #         for x in original_text:
+    #             decrypt_index = alphabet.index(x) - shift_amount
+    #             decrypt_index %= len(alphabet)
+    #             decrypt_message += alphabet[decrypt_index]
+    #         print(decrypt_message)
+
+
+    #     if direction == 'encode':
+    #         encrypt()
+    #     elif direction == 'decode':
+    #         decrypt()
+    #     else:
+    #         input('pick again , wrong choice')
+    #     # encrypt(text, shift) # erfan -2-> gthcp
+    #     # decrypt(text, shift)
+    # caesar() from course:
     def caesar(direction, original_text, shift_amount):
-        def encrypt():
-            encrypt_message = ''
-            for x in original_text:
-                encrypt_index = alphabet.index(x) + shift_amount
-                encrypt_message += alphabet[encrypt_index]
-            print(encrypt_message)
+        if direction == 'decode':
+            shift_amount *= -1   
+
+        encrypt_message = ''
+        for x in original_text:
+            encrypt_index = alphabet.index(x) + shift_amount
+            encrypt_index %= len(alphabet)
+            encrypt_message += alphabet[encrypt_index]
+
+        print(encrypt_message)
 
 
-        def decrypt():
-            decrypt_message = ''
-            for x in original_text:
-                encrypt_index = alphabet.index(x) - shift_amount
-                decrypt_message += alphabet[encrypt_index]
-            print(decrypt_message)
-
-
-        if direction == 'encode':
-            encrypt()
-        elif direction == 'decode':
-            decrypt()
-        else:
-            input('pick again , wrong choice')
-        # encrypt(text, shift) # erfan -2-> gthcp
-        # decrypt(text, shift)
-    
     direction = input("Type 'encode' to encrypt,\nType 'decode' to decrypt:\n").lower()
     text = input("Type your message:\n").lower()
-    shift = int(input("Type the shift number(1-25):\n"))
+    shift = int(input("Type the shift number(any shift number is accepted):\n"))
     caesar(direction, text, shift)
 
 while True:

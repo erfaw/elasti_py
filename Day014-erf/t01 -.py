72a586873b24acac8b0ef3789c0bd45665cea2e2
erfaw <146765011+erfaw@users.noreply.github.com>
2024-12-07 22:39:55 +0330

    added 'calculate_higher()' , and filled 'user_pick' value based on user

--- Day014-erf/t01 -.py
+++ Day014-erf/t01 -.py
@@ -1,10 +1,20 @@
 import art, random
 from clear_sc import clear_screen
 from follower_data import data 
+score = 0
 
 def decide_info():
     '''print infos from A and B to choose at specific situation'''
     print(f"Compare A:\n\t<< {A["name"]}, {A["job"]}, from {A["from"]}. >>{art.vs_logo}Against B:\n\t<< {B["name"]}, {B["job"]}, from {B["from"]}. >>")
+
+
+def calculate_higher(A, B):
+    """returns higher one as a dictionary"""
+    if A["follower"] > B["follower"]:
+        return A
+    else:
+        return B
+    
 
 # showing logo and introduction
 clear_screen()
@@ -18,4 +28,17 @@ B_random_number = random.randint(0, len(data))
 A = data[A_random_number] # a dict
 B = data[B_random_number] # a dict
 
+# ask user to decide more follower
 decide_info()
+
+# fill user_pick based on user opinion
+user_pick = ''
+if input(
+    "Who has more followers? Type 'A' or 'B':  "
+).lower() == 'a' :
+    user_pick = A
+else:
+    user_pick = B
+
+higher_one = calculate_higher(A, B)
+print(higher_one)

d62b02a6bc7590a36218aee6b419b1abbfbd657d
erfaw <146765011+erfaw@users.noreply.github.com>
2024-12-07 22:56:00 +0330

    added while loop for scoring, using 'clear_screen()' right, program now goes well in 'wrong choice' stage

--- Day014-erf/t01 -.py
+++ Day014-erf/t01 -.py
@@ -9,7 +9,7 @@ def decide_info():
 
 
 def calculate_higher(A, B):
-    """returns higher one as a dictionary"""
+    """returns higher one in number of followers as a dictionary"""
     if A["follower"] > B["follower"]:
         return A
     else:
@@ -21,24 +21,42 @@ clear_screen()
 print(art.game_logo)
 print("Welcome to Higher Lower game...")
 
-# pick A and B from data randomly
-A_random_number = random.randint(0, len(data))
-B_random_number = random.randint(0, len(data))
-#build A and B based on random numbers
-A = data[A_random_number] # a dict
-B = data[B_random_number] # a dict
 
-# ask user to decide more follower
-decide_info()
+game_is_over = False
+while not game_is_over:
+    if score >= 1:
+        clear_screen()
+        print(art.game_logo)
+        print(f'right, current score: {score}')
 
-# fill user_pick based on user opinion
-user_pick = ''
-if input(
-    "Who has more followers? Type 'A' or 'B':  "
-).lower() == 'a' :
-    user_pick = A
-else:
-    user_pick = B
+    # pick A and B from data randomly
+    A_random_number = random.randint(0, len(data))
+    B_random_number = random.randint(0, len(data))
+    #build A and B based on random numbers
+    A = data[A_random_number] # a dict
+    B = data[B_random_number] # a dict
 
-higher_one = calculate_higher(A, B)
-print(higher_one)
+    # ask user to decide more follower
+    decide_info()
+
+    # fill user_pick based on user opinion
+    user_pick = ''
+    if input(
+        "Who has more followers? Type 'A' or 'B':  "
+    ).lower() == 'a' :
+        user_pick = A
+    else:
+        user_pick = B
+
+    higher_one = calculate_higher(A, B)
+
+    # user choose right or wrong?
+    if higher_one == user_pick :
+        score += 1
+        # print(f'right, current score: {score}')
+        
+    else: # game should be ended
+        clear_screen()
+        print(art.game_logo)
+        print(f'wrong!!!, final score: {score}\n\n')
+        game_is_over = True

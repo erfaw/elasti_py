907b16d3e548a049ab1a763b818a7ff66e411994
erfaw <146765011+erfaw@users.noreply.github.com>
2024-12-07 22:03:06 +0330

    added 'pick A and B from follower_data randomly'

--- Day014-erf/t01 -.py
+++ Day014-erf/t01 -.py
@@ -8,5 +8,16 @@ clear_screen()
 print(art.game_logo)
 print("Welcome to Higher Lower game...")
 
+# pick A and B from data randomly
+A_random_number = random.randint(0, len(data))
+B_random_number = random.randint(0, len(data))
+#build A and B based on random numbers
+A = data[A_random_number]
+B = data[B_random_number]
+
+print(A)
+print(B)
+
+
 
 

076ba6199b847c2a30b142713aa8ce6cf3375b09
erfaw <146765011+erfaw@users.noreply.github.com>
2025-01-13 23:08:50 +0330

    print report added, print 'in_chanche' added, check kardan inke aya entekhab dakhel menu hast add shd,

--- Day015-erf/t01 - .py
+++ Day015-erf/t01 - .py
@@ -1,3 +1,5 @@
+import subprocess
+
 MENU = {
     "espresso": {
         "ingredients": {
@@ -28,6 +30,38 @@ resources = {
     "water": 300,
     "milk": 200,
     "coffee": 100,
+    "money": 0,
 }
 
-# TODO: 2. salam
+subprocess.call('cls', shell=True) # clear console
+
+user_choice = input(
+    "What would you like? (espresso/latte/cappuccino): "
+)
+
+if user_choice.lower() != 'report' :
+    if user_choice in MENU:
+        print('please insert coins.')
+        quarters = int(input('How many quarters??: '))
+        dimes = int(input('How many dimes??: '))
+        nickles = int(input('How many nickles??: '))
+        pennies = int(input('How many pennies??: '))
+        all_money = 0.25*quarters + 0.10*dimes + 0.05*nickles + 0.01*pennies
+        in_change = all_money - MENU[user_choice.lower()]["cost"]
+        print(f"Here is ${in_change:.2f} in change")
+    else: input(
+        'your choice isnt in menu'
+    )
+else: #print report
+    for x in resources:
+        unit = ''
+        if x == 'water' or x == 'milk':
+            unit = 'ml'
+        elif x == 'coffee':
+            unit = 'g'
+        elif x == 'money': 
+            unit = '$'
+        print(f"{x.title()}: {resources[x]}{unit}")
+
+
+

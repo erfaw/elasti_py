6664ff79bc71c4ae245db10921f151f9fc9ef74c
erfaw <erfawn.h@gmail.com>
2025-01-14 19:41:17 +0330

    function 'print_report()' ro fix kardim ke ba function report ro begirim.

--- Day015-erf/t01 - .py
+++ Day015-erf/t01 - .py
@@ -33,28 +33,10 @@ resources = {
     "money": 0,
 }
 
-subprocess.call('cls', shell=True) # clear console
-
-user_choice = input(
-    "What would you like? (espresso/latte/cappuccino): "
-)
-
-if user_choice.lower() != 'report' :
-    if user_choice in MENU:
-        print('please insert coins.')
-        quarters = int(input('How many quarters??: '))
-        dimes = int(input('How many dimes??: '))
-        nickles = int(input('How many nickles??: '))
-        pennies = int(input('How many pennies??: '))
-        all_money = 0.25*quarters + 0.10*dimes + 0.05*nickles + 0.01*pennies
-        in_change = all_money - MENU[user_choice.lower()]["cost"]
-        print(f"Here is ${in_change:.2f} in change")
-    else: input(
-        'your choice isnt in menu'
-    )
-else: #print report
+def print_report():
+    """print the current report for resources"""
+    unit = ''
     for x in resources:
-        unit = ''
         if x == 'water' or x == 'milk':
             unit = 'ml'
         elif x == 'coffee':
@@ -62,6 +44,30 @@ else: #print report
         elif x == 'money': 
             unit = '$'
         print(f"{x.title()}: {resources[x]}{unit}")
+
+subprocess.call('cls', shell=True) # clear console
+
+user_choice = input(
+    "What would you like? (espresso/latte/cappuccino): "
+)
+
+if user_choice in MENU:
+    print('please insert coins.')
+    quarters = int(input('How many quarters??: '))
+    dimes = int(input('How many dimes??: '))
+    nickles = int(input('How many nickles??: '))
+    pennies = int(input('How many pennies??: '))
+    all_money = 0.25*quarters + 0.10*dimes + 0.05*nickles + 0.01*pennies
+    in_change = all_money - MENU[user_choice.lower()]["cost"]
+    print(f"Here is ${in_change:.2f} in change")
+elif user_choice.lower() == 'report' :
+    print_report()
+else: input(
+    'your choice isnt in menu'
+)
+
+
+
 
 
 

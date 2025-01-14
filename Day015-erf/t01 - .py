c828dde2993196d674ca5b0125dea42b77b470bb
erfaw <erfawn.h@gmail.com>
2025-01-14 21:05:52 +0330

    function 'update_resources()' add shd, ke chizayi ke masraf shde ro az resource mines mikne o money ro baste be cost ezafe mikne. va barname toye halghe While True gozashte shd repeatable shd. albate hanoz baraye halati ke resource ha tamom mishe va nmitone hichkodom ro drst kne chizi naneveshtim mitone feature badish bashe.

--- Day015-erf/t01 - .py
+++ Day015-erf/t01 - .py
@@ -60,37 +60,49 @@ def is_enough_money(order, paid_money):
     else: 
         return True
 
+def update_resources(order):
+    """nesbat be order anjam shde mavared masrafi ro az 'resources' kam mikne va money i ke bedast omade ezafe mikne."""
+    used_resources = MENU[order]["ingredients"]
+    #chizaye masraf shde:
+    for r in used_resources:
+        resources[r] -= used_resources[r]
+    # poole ezafe shde
+    resources["money"] += MENU[order]["cost"]
+    
+
 subprocess.call('cls', shell=True) # clear console
 
-user_choice = input(
-    "What would you like? (espresso/latte/cappuccino): "
-)
+while True:
+    user_choice = input(
+        "What would you like? (espresso/latte/cappuccino): "
+    )
 
-if user_choice in MENU:
-    if not is_possible_to_make(user_choice):
-        input ("Sorry, machine is out of resources!!!")
-    else:
-        print('please insert coins.')
-        quarters = int(input('How many quarters?: '))
-        dimes = int(input('How many dimes?: '))
-        nickles = int(input('How many nickles?: '))
-        pennies = int(input('How many pennies?: '))
-        all_money = 0.25*quarters + 0.10*dimes + 0.05*nickles + 0.01*pennies # inja pool be vahed dollar tabdil mishe. chera ke cost order ha hm be dollare
-        if not is_enough_money(user_choice, all_money):
-            input ("Sorry, these coins aren't enough...")
-        else: # inja hame shart ha check shdn o tamome. 
-            in_change = all_money - MENU[user_choice.lower()]["cost"] # change is money which left from your paid money
-            print(f"Here is ${in_change:.2f} in change")
-            print(f"here is your {user_choice}, Enjoy! :) ")
-elif user_choice.lower() == 'report' :
-    print_report()
-else: input(
-    'your choice isnt in menu'
-)
+    if user_choice in MENU:
+        if not is_possible_to_make(user_choice):
+            input ("Sorry, machine is out of resources!!!")
+        else:
+            print('please insert coins.')
+            quarters = int(input('How many quarters?: '))
+            dimes = int(input('How many dimes?: '))
+            nickles = int(input('How many nickles?: '))
+            pennies = int(input('How many pennies?: '))
+            all_money = 0.25*quarters + 0.10*dimes + 0.05*nickles + 0.01*pennies # inja pool be vahed dollar tabdil mishe. chera ke cost order ha hm be dollare, arzesh har type i az coin ha hm baghaleshon zarb shde ke be dollar tabdil beshan
+            if not is_enough_money(user_choice, all_money):
+                input ("Sorry, these coins aren't enough...")
+            else: # inja hame shart ha check shdn o tamome. 
+                in_change = all_money - MENU[user_choice.lower()]["cost"] # change is money which left from your paid money
+                update_resources(user_choice)
+                print(f"Here is ${in_change:.2f} in change")
+                print(f"here is your {user_choice}, Enjoy! :) ")
+    elif user_choice.lower() == 'report' :
+        print_report()
+    else: input(
+        'your choice isnt in menu'
+    )
 
 
 # DONE 1. check kardan inke aya user_choice ba resource haye feli ke darim ghable anjame ya na. 
-# TODO 2. check kardan inke aya pooli ke sekke i vared shde kafie ya na
+# DONE 2. check kardan inke aya pooli ke sekke i vared shde kafie ya na
 # TODO 3. coffee ro drst mikne dastgah , bayad be mizan chizi ke drst karde az resource ha kam beshe
 # TODO 4. vaghti sefaresh tamom shd, amade sefaresh badi beshe. 
 # TODO 5. agar resource ha be hadi nist ke hichkodom az taste haro drst kne benevise ke nadarim . (plus)

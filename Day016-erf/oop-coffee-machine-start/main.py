04d897683d83ed75ba3a51aa2e4abf0b772bfa06
erfaw <erfawn.h@gmail.com>
2025-01-17 23:26:29 +0330

    coding , object of drinks made, doing MENU right now, (at 007 of day016)

--- Day016-erf/oop-coffee-machine-start/main.py
+++ Day016-erf/oop-coffee-machine-start/main.py
@@ -1,3 +1,12 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+latte = MenuItem(name= 'latte', water= 200, milk= 150, coffee= 24, cost= 2.5)
+
+espresso = MenuItem(name='espresso', water=50, milk=0, coffee=18, cost=1.5)
+
+capppuccino = MenuItem(name='capppuccino', water=250, milk=100, coffee=24, cost=3.0)
+
+my_menu = Menu()
+my_menu.get_items()

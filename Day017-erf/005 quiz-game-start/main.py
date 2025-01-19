87e6de75858a259a06cd0a8d3767df1e6b1b3bcb
erfaw <erfawn.h@gmail.com>
2025-01-19 20:54:17 +0330

    on 'main.py' file>> halghe for baryae sakhte 'question_bank' neveshte shd,'question_bank' sakhte shd.

--- Day017-erf/005 quiz-game-start/main.py
+++ Day017-erf/005 quiz-game-start/main.py
@@ -1,3 +1,14 @@
 from data import question_data
 from question_model import Question
 
+question_bank = [] # its a list contains objects of questions 
+
+for each_dict in question_data: #make and append question objects to 'question_bank' list.
+    question_bank.append(
+        Question(
+            each_dict['text'] ,
+            each_dict['answer']
+        )
+    )
+
+

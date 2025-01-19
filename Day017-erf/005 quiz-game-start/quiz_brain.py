bf4b4f2666daed970f01994fb1af76ce9b81a922
erfaw <erfawn.h@gmail.com>
2025-01-19 21:51:48 +0330

    'quiz_brain.py' >> 1. ezafe shdn 'self.question_number' ke nabod eslah shdn.. 2. methode 'still_has_questions()' sakhte shd.

--- Day017-erf/005 quiz-game-start/quiz_brain.py
+++ Day017-erf/005 quiz-game-start/quiz_brain.py
@@ -4,7 +4,17 @@ class QuizBrain:
         self.question_number = 0
         self.question_list = q_list
 
+    def still_has_questions(self):
+        """return a boolian , True >> there is more question to ask, False >> the quiz ran out of questions."""
+        self.q_list_len = len(self.question_list)
+        if (self.question_number + 1) >= self.q_list_len:
+            return False
+        else: 
+            return True
+
+
     def next_question(self):
         self.user_answer = input(
-            f"Q.{self.question_number + 1}: {self.question_list[self.question_number].text} (true/false)"
+            f"Q.{self.question_number + 1}: {self.question_list[self.question_number].text} (true/false): "
         )
+        self.question_number += 1

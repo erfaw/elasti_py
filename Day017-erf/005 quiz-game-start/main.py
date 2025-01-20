9633e85e865727c5a78bfb00a62a70510ec38190
erfaw <erfawn.h@gmail.com>
2025-01-20 21:10:36 +0330

    'main.py' >> fix kardan code baraye estefade kardan az 'data2.py'.

--- Day017-erf/005 quiz-game-start/main.py
+++ Day017-erf/005 quiz-game-start/main.py
@@ -1,18 +1,27 @@
-from data import question_data
+from data2 import question_data
+# from data import question_data
 from question_model import Question
 from quiz_brain import QuizBrain
+import subprocess
 
 question_bank = [] # its a list contains objects of questions 
 
 for each_dict in question_data: #make and append question objects to 'question_bank' list.
     question_bank.append(
-        Question(
-            each_dict['text'] ,
-            each_dict['answer']
+        Question( # for 'data2.py' file
+            each_dict['question'] ,
+            each_dict['correct_answer']
         )
+        # Question( # for 'data.py' file
+        #     each_dict['text'] ,
+        #     each_dict['answer']
+        # )
     )
 
 quiz = QuizBrain(question_bank)
+
+subprocess.call('cls', shell=True)
+
 while quiz.still_has_questions():
     quiz.next_question()
 

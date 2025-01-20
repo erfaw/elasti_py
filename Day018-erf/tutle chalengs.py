2baf54a9b3ee578c66bded1fffaaf6ec36994e8c
erfaw <erfawn.h@gmail.com>
2025-01-20 22:01:48 +0330

    made Day018 folder, made 'turtle chalenges.py' file, made pre codes to continue day018

---
+++ Day018-erf/tutle chalengs.py
@@ -0,0 +1,10 @@
+from turtle import Turtle, Screen
+
+arshia = Turtle()
+arshia.shape('turtle')
+arshia.color('blue')
+
+
+# baraye inke nemayesh bedim o ba click az safhe kharej beshe :
+my_screen = Screen()
+my_screen.exitonclick()

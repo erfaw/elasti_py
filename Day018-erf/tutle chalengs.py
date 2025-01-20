4108abd82fa0d593a122c962c48acfdabf27b8e3
erfaw <erfawn.h@gmail.com>
2025-01-20 22:15:33 +0330

    turtle draw a simple square

--- Day018-erf/tutle chalengs.py
+++ Day018-erf/tutle chalengs.py
@@ -4,6 +4,12 @@ arshia = Turtle()
 arshia.shape('turtle')
 arshia.color('blue')
 
+# Draw a Square 
+arshia.forward(100)
+for i in range(3):
+    arshia.right(90)
+    arshia.forward(100)
+
 
 # baraye inke nemayesh bedim o ba click az safhe kharej beshe :
 my_screen = Screen()

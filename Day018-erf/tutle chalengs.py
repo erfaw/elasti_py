66bc720ec0c0bc3ee9b27b9efa32b582ee2783d0
erfaw <erfawn.h@gmail.com>
2025-01-21 22:45:45 +0330

    add draw triangle, square, pentagon, ..., decagon with a side common

--- Day018-erf/tutle chalengs.py
+++ Day018-erf/tutle chalengs.py
@@ -22,8 +22,20 @@ def draw_dashed_line(how_many):
         tim.forward(20)
         tim.penup()
         tim.forward(20)
+# draw_dashed_line(15)
 
-draw_dashed_line(15)
+# draw triangle, square, pentagon, ..., decagon with a side common
+colors = ['red', 'green', 'blue', 'orange', 'black', 'purple', 'DarkRed', 'yellow',]
+for number_side in range(3,10): # 3 for triangle, 4 for square etc. 
+    tim.setpos(0,0)
+    tim.pencolor(
+        colors[number_side-3] # get right index 
+    )
+    for _ in range(number_side): # drawing based on how many sides the shape has
+        tim.forward(100)
+        tim.right(360/number_side)
+
+
 
 # baraye inke nemayesh bedim o ba click az safhe kharej beshe :
 my_screen = Screen()

7f546b92c1032cfb83948258482fe530696f5727
erfaw <erfawn.h@gmail.com>
2025-01-21 22:22:20 +0330

    add 'draw_dashed_line()' function .

--- Day018-erf/tutle chalengs.py
+++ Day018-erf/tutle chalengs.py
@@ -1,15 +1,29 @@
 from turtle import Turtle, Screen
 
-arshia = Turtle()
-arshia.shape('turtle')
-arshia.color('blue')
+tim = Turtle()
+tim.shape('turtle')
+tim.color('blue')
 
 # Draw a Square 
-arshia.forward(100)
-for i in range(3):
-    arshia.right(90)
-    arshia.forward(100)
+def draw_square():
+    """draw a square with tim"""
+    tim.forward(100)
+    for i in range(3):
+        tim.right(90)
+        tim.forward(100)
 
+# Draw a dashed line:
+def draw_dashed_line(how_many):
+    """draw a dashed line with turtle."""
+    tim.penup()
+    tim.setpos(-300, 0) # change first position of turtle to -100x.
+    for _ in range(how_many):
+        tim.pendown()
+        tim.forward(20)
+        tim.penup()
+        tim.forward(20)
+
+draw_dashed_line(15)
 
 # baraye inke nemayesh bedim o ba click az safhe kharej beshe :
 my_screen = Screen()

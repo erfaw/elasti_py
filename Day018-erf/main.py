bb901bc2a716b0ce55b9cb81948f075f4de1fece
erfaw <erfawn.h@gmail.com>
2025-01-24 20:57:36 +0330

    ta inja az code i ke comment shde khoroji gereftim rang haro va code ro dige estefade nmiknim o karemon ba hamon khoroji hast. alan ye commit mizarim ke edame ro berim...

--- Day018-erf/main.py
+++ Day018-erf/main.py
@@ -1,15 +1,17 @@
 import colorgram
 
-colors_of_pic = colorgram.extract(r"C:\Users\ErF\Desktop\python\elasti_py\Day018-erf\image.jpg", 10)
+colors_of_pic = colorgram.extract(r"C:\Users\ErF\Desktop\python\elasti_py\Day018-erf\image.jpg", 30)
 
-colors = []
+colors = [
+    (253, 251, 247), (253, 248, 252), (235, 252, 243), (198, 13, 32), (248, 236, 25), (40, 76, 188), (244, 247, 253), (39, 216, 69), (238, 227, 5), (227, 159, 49), (29, 40, 154), (212, 76, 15), (17, 153, 17), (241, 36, 161), (195, 16, 12), (223, 21, 120), (68, 10, 31), (61, 15, 8), (223, 141, 206), (11, 97, 62), (219, 159, 11), (54, 209, 229), (19, 21, 49), (238, 157, 216), (79, 74, 212), (10, 228, 238), (73, 212, 168), (93, 233, 198), (65, 231, 239), (217, 88, 51)
+]
 
-for each in colors_of_pic:
-    colors_list = []
-    colors_list.append(each.rgb.r)
-    colors_list.append(each.rgb.g)
-    colors_list.append(each.rgb.b)
-    colors.append(tuple(colors_list))
+# for each in colors_of_pic:
+#     colors_list = []
+#     colors_list.append(each.rgb.r)
+#     colors_list.append(each.rgb.g)
+#     colors_list.append(each.rgb.b)
+#     colors.append(tuple(colors_list))
 
-print(colors)
+# print(colors)
 

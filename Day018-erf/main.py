9e71335f047ed65c518734298113522df287abcf
erfaw <erfawn.h@gmail.com>
2025-01-24 22:26:06 +0330

    comment haye tolid 'colors[]' delete shd.

--- Day018-erf/main.py
+++ Day018-erf/main.py
@@ -7,16 +7,6 @@ colors = [
     (198, 13, 32), (248, 236, 25), (40, 76, 188), (244, 247, 253), (39, 216, 69), (238, 227, 5), (227, 159, 49), (29, 40, 154), (212, 76, 15), (17, 153, 17), (241, 36, 161), (195, 16, 12), (223, 21, 120), (68, 10, 31), (61, 15, 8), (223, 141, 206), (11, 97, 62), (219, 159, 11), (54, 209, 229), (19, 21, 49), (238, 157, 216), (79, 74, 212), (10, 228, 238), (73, 212, 168), (93, 233, 198), (65, 231, 239), (217, 88, 51)
 ]
 
-# for each in colors_of_pic:
-#     colors_list = []
-#     colors_list.append(each.rgb.r)
-#     colors_list.append(each.rgb.g)
-#     colors_list.append(each.rgb.b)
-#     colors.append(tuple(colors_list))
-
-# print(colors)
-
-# making our turtle <tim>
 tim = turtle.Turtle()
 tim.penup()
 tim.setpos(-300, -250)

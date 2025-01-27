9cc2d596272b81c69eeba8908fad51ca525f1280
erfaw <erfawn.h@gmail.com>
2025-01-27 21:12:35 +0330

    file Day019 sakhte shd, event listener haye 'a', 's', 'd', 'w' va 'c' add shdn.

---
+++ Day019-erf/t19.py
@@ -0,0 +1,27 @@
+from turtle import Turtle, Screen
+
+arsh = Turtle()
+my_sc = Screen()
+
+def move_forward():
+    arsh.forward(20)
+    
+def move_backward():
+    arsh.backward(20)
+    
+def turn_left():
+    arsh.left(10)
+    
+def turn_right():
+    arsh.right(10)
+    
+def clear_drawing():
+    arsh.clear()
+
+my_sc.onkey(move_forward, "w")
+my_sc.onkey(move_backward, "s")
+my_sc.onkey(turn_left, "a")
+my_sc.onkey(turn_right, "d")
+my_sc.onkey(clear_drawing, "c")
+my_sc.listen()
+my_sc.exitonclick()

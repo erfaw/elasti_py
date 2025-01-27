2e6f2b8decadba11510f46b938cbfb048d7de60a
erfaw <erfawn.h@gmail.com>
2025-01-27 21:59:11 +0330

    ta day019 005 10:38.

--- Day019-erf/main.py
+++ Day019-erf/main.py
@@ -3,6 +3,7 @@ my_sc = Screen()
 my_sc.setup(width= 800, height=500)
 #build 6 turtle
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+
 trtl_1 = Turtle(shape="turtle")
 trtl_2 = Turtle(shape="turtle")
 trtl_3 = Turtle(shape="turtle")
@@ -10,6 +11,7 @@ trtl_4 = Turtle(shape="turtle")
 trtl_5 = Turtle(shape="turtle")
 trtl_6 = Turtle(shape="turtle")
 # set colors
+
 # Move them to left side , lined.
 
 

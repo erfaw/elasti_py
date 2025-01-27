db4a96ce9da5ec0acdf3b122d14d1825e21fd82d
erfaw <erfawn.h@gmail.com>
2025-01-27 21:56:21 +0330

    vaghti .color() ro inline heyn initialize anjam midi method ha gheyr fa'al mishan, bayad joda kar anjam beshe.

--- Day019-erf/main.py
+++ Day019-erf/main.py
@@ -1,12 +1,16 @@
 from turtle import Turtle, Screen
 my_sc = Screen()
+my_sc.setup(width= 800, height=500)
 #build 6 turtle
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
-trtl_1 = Turtle(shape="turtle").color(colors[0])
-trtl_2 = Turtle(shape="turtle").color(colors[1])
-trtl_3 = Turtle(shape="turtle").color(colors[2])
-trtl_4 = Turtle(shape="turtle").color(colors[3])
-trtl_5 = Turtle(shape="turtle").color(colors[4])
-trtl_6 = Turtle(shape="turtle").color(colors[5])
+trtl_1 = Turtle(shape="turtle")
+trtl_2 = Turtle(shape="turtle")
+trtl_3 = Turtle(shape="turtle")
+trtl_4 = Turtle(shape="turtle")
+trtl_5 = Turtle(shape="turtle")
+trtl_6 = Turtle(shape="turtle")
+# set colors
+# Move them to left side , lined.
+
 
 my_sc.exitonclick()

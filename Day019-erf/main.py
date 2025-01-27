2836d12082d1560638984aa10915f868cc08508c
erfaw <erfawn.h@gmail.com>
2025-01-27 21:43:52 +0330

    hame turtle ha sakhte shdn o shape o rangeshon fix shd.

--- Day019-erf/main.py
+++ Day019-erf/main.py
@@ -1,4 +1,12 @@
 from turtle import Turtle, Screen
-
+my_sc = Screen()
 #build 6 turtle
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+trtl_1 = Turtle(shape="turtle").color(colors[0])
+trtl_2 = Turtle(shape="turtle").color(colors[1])
+trtl_3 = Turtle(shape="turtle").color(colors[2])
+trtl_4 = Turtle(shape="turtle").color(colors[3])
+trtl_5 = Turtle(shape="turtle").color(colors[4])
+trtl_6 = Turtle(shape="turtle").color(colors[5])
+
+my_sc.exitonclick()

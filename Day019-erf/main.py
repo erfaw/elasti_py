02e42f773c7fd83808c6bbf133a11aff68741d0c
erfaw <erfawn.h@gmail.com>
2025-01-28 23:21:56 +0330

    add some comments, write some code to a better input user bet with a while loop, NEXT: process to race

--- Day019-erf/main.py
+++ Day019-erf/main.py
@@ -2,10 +2,18 @@ from turtle import Turtle, Screen
 import random
 my_sc = Screen()
 my_sc.setup(width= 500, height=400)
-
-user_bet = my_sc.textinput(title="Make a bet", prompt="which turtle will be first one(color)?")
-#build 6 turtle
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+
+is_race_on = False
+
+not_catched = True
+while not_catched: 
+    user_bet = my_sc.textinput(title="Make a bet", prompt=f"which turtle will be first one({" ,".join(colors)})?")
+    if user_bet.lower() in colors:
+        is_race_on = True
+        not_catched = False
+
+#build 6 turtle
 
 current_x = my_sc.window_width()
 current_y = -(my_sc.window_height()/2) + 50
@@ -21,6 +29,7 @@ for _ in range(6): # a loop for make, set color, set right position to turtles a
     trtl.color(colors[_])
     trtl.goto(x= -(current_x/2)+40, y=true_height())
     turtles.append(trtl)
+
 
 
 my_sc.exitonclick()

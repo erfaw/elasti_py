81cd2cc4f49b1d8dd8020d6578436cd723a356ad
erfaw <erfawn.h@gmail.com>
2025-01-28 22:10:37 +0330

    turtle ha dar jaye drst be khat shdn, vali moshkel injast ke hanoz rangeshon fix nist va inke vase be khat kardaneshon va harkodom code zadim, fekr nmiknm in drst bashe, toye 'main2.py' test miknim ravesh halghe for ro. nashod barmigardim haminja.

--- Day019-erf/main.py
+++ Day019-erf/main.py
@@ -4,12 +4,25 @@ my_sc.setup(width= 800, height=500)
 #build 6 turtle
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 
+current_x = my_sc.window_width()
+current_y = -(my_sc.window_height()/2) + 50
+def true_height():
+    global current_y
+    current_y += 50
+    return current_y - 50
+
 trtl_1 = Turtle(shape="turtle")
+trtl_1.goto(x= -(current_x/2)+40, y=true_height())
 trtl_2 = Turtle(shape="turtle")
+trtl_2.goto(x= -(current_x/2)+40, y=true_height())
 trtl_3 = Turtle(shape="turtle")
+trtl_3.goto(x= -(current_x/2)+40, y=true_height())
 trtl_4 = Turtle(shape="turtle")
+trtl_4.goto(x= -(current_x/2)+40, y=true_height())
 trtl_5 = Turtle(shape="turtle")
+trtl_5.goto(x= -(current_x/2)+40, y=true_height())
 trtl_6 = Turtle(shape="turtle")
+trtl_6.goto(x= -(current_x/2)+40, y=true_height())
 # set colors
 
 # Move them to left side , lined.

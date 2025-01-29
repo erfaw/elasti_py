abf8701aecacce2b30be63b74101b5469c9a899f
erfaw <erfawn.h@gmail.com>
2025-01-29 21:54:02 +0330

    mikham beram roye branch <testmain> va test knm bbinm mitonm ye method be turtle ha ezafe knm ke position turtle ghabl ro begire o bere onja va turtle ghabl hm position khodesho return kne vase baadi ya kheyr.

--- Day020-erf/Snakes_game/main.py
+++ Day020-erf/Snakes_game/main.py
@@ -18,12 +18,28 @@ turtles[0].color('orange')
 
 #make turtle move forward
 is_game_over = False
-def move():
-    for turtle in turtles:
-        turtle.fd(25)
+tr=''
+def h_left():
+    tr.setheading(180)
+def h_right():
+    tr.setheading(0)
+def h_up():
+    tr.setheading(90)
+def h_down():
+    tr.setheading(270)  
+my_sc.onkey(fun=h_up, key="w")
+my_sc.onkey(fun=h_left, key="a")
+my_sc.onkey(fun=h_down, key="s")
+my_sc.onkey(fun=h_right, key="d")
+my_sc.listen()
+position = ''
 while not is_game_over:
+    position = turtles[0].pos()
+    turtles[0].fd(3)
     for turtle in turtles:
-        turtle.fd(3)
+        turtle.goto(position)
+
+        
 
 
 

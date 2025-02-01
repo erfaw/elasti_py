480337cbe318745b70ae2a42301719e4bd1b49b6
erfaw <erfawn.h@gmail.com>
2025-02-01 22:22:46 +0330

    branch>> testmain. :: function 'call_back_move()' sakhte shd ke kari mikne ina az positioin nafar baad taba'iat knn va ma turtle[0] ro control knim o drst harekat knn. 2 inke kari krdim agar dare mire rast natone bere chap va belaks o dar jahat haye dige hm...

--- Day020-erf/Snakes_game/main.py
+++ Day020-erf/Snakes_game/main.py
@@ -3,7 +3,7 @@ tim = Turtle()
 my_sc = Screen()
 my_sc.bgcolor('black')
 turtles = []
-for turtle_index in range(3): # make the body of snake (3 square)
+for turtle_index in range(10): # make the body of snake (3 square)
     new_turtle = Turtle('square')
     new_turtle.penup()
     new_turtle.color('white')
@@ -14,39 +14,62 @@ for turtle_index in range(3): # make the body of snake (3 square)
         )
     turtles.append(new_turtle)
 
-# turtles[0].shape('triangle')
-# turtles[0].color('orange')
+turtles[0].shape('triangle')
+turtles[0].color('orange')
+turtles[0].speed("fast")
 
 #make turtle move forward
 is_game_over = False
 tr=''
 def h_left():
-    turtles[0].setheading(180)
+    if not turtles[0].heading() == 0 :
+        turtles[0].setheading(180)
+    else: pass
 def h_right():
-    turtles[0].setheading(0)
+    if not turtles[0].heading() == 180 :
+        turtles[0].setheading(0)
+    else: pass
 def h_up():
-    turtles[0].setheading(90)
+    if not turtles[0].heading() == 270 :
+        turtles[0].setheading(90)
+    else: pass    
 
 def h_down():
-    turtles[0].setheading(270)  
+    if not turtles[0].heading() == 90 :
+        turtles[0].setheading(270)
+    else: pass 
 my_sc.onkey(fun=h_up, key="w")
 my_sc.onkey(fun=h_left, key="a")
 my_sc.onkey(fun=h_down, key="s")
 my_sc.onkey(fun=h_right, key="d")
 my_sc.listen()
 
+# mikhaym ye function callback benevisim ke jaye feli ro bede be ghabli va ghabli goto beshe onja va hamintori ta enteha
+counter_call_back = 1
+def call_back_move(x_togo, y_togo):
+    """ye callback function hast ke ba estefade az oon position e ghabl az fd(10) ro negah midarim o baad az fd(10) roye turtle[0] pass midim be turtle badi va hminjori ta enteha, ke dar enteha baes avali 10 ta harekat kne va ma ba onkey() ha on avali ro control miknim, baghie az on avali tabaiat miknn zanjire var"""
+    global counter_call_back
+    if counter_call_back <= len(turtles)-1:
+        prev_position_x = turtles[counter_call_back].xcor()
+        prev_position_y = turtles[counter_call_back].ycor()
+        turtles[counter_call_back].goto(
+            x_togo, y_togo
+        )
+        counter_call_back += 1
+        call_back_move(
+            prev_position_x, prev_position_y
+        )
+    else:
+        counter_call_back = 1
+
 while not is_game_over:
-    i=2
+    first_position_x = turtles[0].xcor()
+    first_position_y = turtles[0].ycor()
     turtles[0].fd(10)
-
-    for tr in turtles:
-        tr.fd(10)
-
+    
+    call_back_move(
+        first_position_x, first_position_y
+    )
             
-
-def new_method():
-    pass
-
-
 
 my_sc.exitonclick()

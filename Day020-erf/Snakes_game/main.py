4e7879af2dcd381fd7a7fdfc8f3db33f1974c637
erfaw <erfawn.h@gmail.com>
2025-02-01 22:31:13 +0330

    changes: 1.all new turtles speed setted to 'fastest', 2.first positioning for turtles setted to -18 from last one.

--- Day020-erf/Snakes_game/main.py
+++ Day020-erf/Snakes_game/main.py
@@ -7,16 +7,16 @@ for turtle_index in range(10): # make the body of snake (3 square)
     new_turtle = Turtle('square')
     new_turtle.penup()
     new_turtle.color('white')
-    # new_turtle.speed("slow")
+    new_turtle.speed("fastest")
     if not turtle_index == 0:
         new_turtle.goto(
-            turtles[turtle_index-1].xcor()-20, 0
+            turtles[turtle_index-1].xcor()-18, 0
         )
     turtles.append(new_turtle)
 
 turtles[0].shape('triangle')
 turtles[0].color('orange')
-turtles[0].speed("fast")
+# turtles[0].speed("fastest")
 
 #make turtle move forward
 is_game_over = False

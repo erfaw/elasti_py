2ffd96cb7a8977d86a6e6cb28d7398a097957eb7
erfaw <erfawn.h@gmail.com>
2025-02-01 23:07:55 +0330

    1. add some comments and remove some comments, 2.set screen.setup() for width and height = 550, 3.create a food object and positioned it to a random place (-250, 250)

--- Day020-erf/Snakes_game/main.py
+++ Day020-erf/Snakes_game/main.py
@@ -1,9 +1,14 @@
 from turtle import Turtle, Screen
-tim = Turtle()
+import random
 my_sc = Screen()
 my_sc.bgcolor('black')
+my_sc.setup(width=550, height=550)
+
+score = 0
+
+# make the body of snake (4 square)
 turtles = []
-for turtle_index in range(10): # make the body of snake (3 square)
+for turtle_index in range(4): 
     new_turtle = Turtle('square')
     new_turtle.penup()
     new_turtle.color('white')
@@ -14,37 +19,41 @@ for turtle_index in range(10): # make the body of snake (3 square)
         )
     turtles.append(new_turtle)
 
+# set specific color and shape to head of snake
 turtles[0].shape('triangle')
 turtles[0].color('orange')
-# turtles[0].speed("fastest")
 
-#make turtle move forward
+# some variable and function to move turtle
 is_game_over = False
-tr=''
 def h_left():
+    """set heading to """
     if not turtles[0].heading() == 0 :
         turtles[0].setheading(180)
     else: pass
 def h_right():
+    """set heading to """
+    
     if not turtles[0].heading() == 180 :
         turtles[0].setheading(0)
     else: pass
 def h_up():
+    """set heading to """
     if not turtles[0].heading() == 270 :
         turtles[0].setheading(90)
     else: pass    
-
 def h_down():
+    """set heading to """
     if not turtles[0].heading() == 90 :
         turtles[0].setheading(270)
     else: pass 
+
+# adding event listener to some keys (W,A,S,D) to direct turtle
 my_sc.onkey(fun=h_up, key="w")
 my_sc.onkey(fun=h_left, key="a")
 my_sc.onkey(fun=h_down, key="s")
 my_sc.onkey(fun=h_right, key="d")
 my_sc.listen()
 
-# mikhaym ye function callback benevisim ke jaye feli ro bede be ghabli va ghabli goto beshe onja va hamintori ta enteha
 counter_call_back = 1
 def call_back_move(x_togo, y_togo):
     """ye callback function hast ke ba estefade az oon position e ghabl az fd(10) ro negah midarim o baad az fd(10) roye turtle[0] pass midim be turtle badi va hminjori ta enteha, ke dar enteha baes avali 10 ta harekat kne va ma ba onkey() ha on avali ro control miknim, baghie az on avali tabaiat miknn zanjire var"""
@@ -62,6 +71,19 @@ def call_back_move(x_togo, y_togo):
     else:
         counter_call_back = 1
 
+# add food by random
+food = Turtle('circle')
+food.penup()
+food.color('blue')
+food.shapesize(0.7, 0.7)
+food.goto(
+    random.randint(-250, 250),
+    random.randint(-250, 250)
+)
+food_position = food.pos()
+
+
+# Loop baraye harekat khodkar be jelo
 while not is_game_over:
     first_position_x = turtles[0].xcor()
     first_position_y = turtles[0].ycor()

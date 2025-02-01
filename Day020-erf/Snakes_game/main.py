0e7d7da15b47dbbb54d47db851965f99e6eda922
erfaw <erfawn.h@gmail.com>
2025-02-01 21:47:04 +0330

    nothing for checkout

--- Day020-erf/Snakes_game/main.py
+++ Day020-erf/Snakes_game/main.py
@@ -7,39 +7,45 @@ for turtle_index in range(3): # make the body of snake (3 square)
     new_turtle = Turtle('square')
     new_turtle.penup()
     new_turtle.color('white')
+    # new_turtle.speed("slow")
     if not turtle_index == 0:
         new_turtle.goto(
-            turtles[turtle_index-1].xcor()-18, 0
+            turtles[turtle_index-1].xcor()-20, 0
         )
     turtles.append(new_turtle)
 
-turtles[0].shape('triangle')
-turtles[0].color('orange')
+# turtles[0].shape('triangle')
+# turtles[0].color('orange')
 
 #make turtle move forward
 is_game_over = False
 tr=''
 def h_left():
-    tr.setheading(180)
+    turtles[0].setheading(180)
 def h_right():
-    tr.setheading(0)
+    turtles[0].setheading(0)
 def h_up():
-    tr.setheading(90)
+    turtles[0].setheading(90)
+
 def h_down():
-    tr.setheading(270)  
+    turtles[0].setheading(270)  
 my_sc.onkey(fun=h_up, key="w")
 my_sc.onkey(fun=h_left, key="a")
 my_sc.onkey(fun=h_down, key="s")
 my_sc.onkey(fun=h_right, key="d")
 my_sc.listen()
-position = ''
-while not is_game_over:
-    position = turtles[0].pos()
-    turtles[0].fd(3)
-    for turtle in turtles:
-        turtle.goto(position)
 
-        
+while not is_game_over:
+    i=2
+    turtles[0].fd(10)
+
+    for tr in turtles:
+        tr.fd(10)
+
+            
+
+def new_method():
+    pass
 
 
 

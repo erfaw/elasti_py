57aac11bfe359723a21f336b88b9baef0f674687
erfaw <erfawn.h@gmail.com>
2025-02-03 21:58:42 +0330

    function 'make_new_turtle()' ezafe shd, code hash az for e avalie baraye sakhte 4 square badaneye snake delete shd va 'make_new_turtle()' jaygozin shd.

--- Day020-erf/Snakes_game/main.py
+++ Day020-erf/Snakes_game/main.py
@@ -8,12 +8,16 @@ score = 0
 
 # make the body of snake (4 square)
 turtles = []
-for turtle_index in range(4): 
+def make_new_turtle():
     new_turtle = Turtle('square')
     new_turtle.penup()
     new_turtle.color('white')
     new_turtle.speed("fastest")
     new_turtle.shapesize(0.5,0.5)
+    return new_turtle
+    
+for turtle_index in range(4): 
+    new_turtle = make_new_turtle()
 
     if not turtle_index == 0:
         new_turtle.goto(

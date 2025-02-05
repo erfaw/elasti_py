3deca9a4df4a939e8efd75f177eab74e771cef3a
erfaw <erfawn.h@gmail.com>
2025-02-05 21:17:01 +0330

    class snake ba course match shd.

--- Day020-erf/Snakes_game/snake.py
+++ Day020-erf/Snakes_game/snake.py
@@ -1,32 +1,32 @@
 from turtle import Turtle
 
+STARTING_POSITION = [(0,0), (-20,0), (-40,0), (-60,0)]
+MOVE_DISTANCE = 20
+
 class Snake():
+    def __init__(self):
+        self.turtles = []
+        self.create_snake()
+
+    def create_snake(self):
+        for position in STARTING_POSITION:
+            new_turtle = self.make_new_turtle()
+            new_turtle.goto(position)
+            self.turtles.append(new_turtle)
+        
     def make_new_turtle(self):
         """make a turtle with white color and fastest speed and shapesize 0.5, 0.5 and return that object"""
         new_turtle = Turtle('square')
         new_turtle.penup()
         new_turtle.color('white')
-        new_turtle.speed("fastest")
-        new_turtle.shapesize(0.5,0.5)
+        # new_turtle.speed("fastest")
+        # new_turtle.shapesize(0.5,0.5)
         return new_turtle
-    
-    def __init__(self):
-        self.turtles = []
-        for turtle_index in range(4): 
-            new_turtle = self.make_new_turtle()
-            if not turtle_index == 0:
-                new_turtle.goto(
-                    self.turtles[turtle_index-1].xcor()-18, 0
-                )
-            self.turtles.append(new_turtle)
-            # set specific color and shape to head of snake
-            self.turtles[0].shape('triangle')
-            self.turtles[0].color('orange')
 
     def move(self):
         first_position_x = self.turtles[0].xcor()
         first_position_y = self.turtles[0].ycor()    
-        self.turtles[0].fd(10)
+        self.turtles[0].fd(MOVE_DISTANCE)
         raw_prev_position = 0
         for snake_body in range(len(self.turtles)) :
             if snake_body == 0 : 

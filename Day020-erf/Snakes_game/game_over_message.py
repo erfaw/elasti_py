90377a657b15ee00477e0f332e88e1d3ae98a42d
erfaw <erfawn.h@gmail.com>
2025-02-15 21:48:19 +0330

    change folder name 'Day020-erf' to 'Day020-erf & Day021-erf'

--- Day020-erf/Snakes_game/game_over_message.py
+++
@@ -1,9 +0,0 @@
-from turtle import Turtle
-class Game_over_message:
-    def __init__(self):
-        pass
-        
-    def make_game_over_message(self):
-        msg = Turtle()
-        msg.pencolor('white')
-        msg.write("GAME OVER !!!", False, "center", ("Arial", 40, "bold"))

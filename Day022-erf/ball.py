fd32b7ac137dc4e5de15aa7b40c79ae8be97394d
erfaw <erfawn.h@gmail.com>
2025-02-16 22:16:15 +0330

    file 'ball.py' baraye sakhte class Ball sakhte shd. docstring ezafe shd. az Turtle inherit shd.

---
+++ Day022-erf/ball.py
@@ -0,0 +1,7 @@
+from turtle import Turtle
+
+class Ball(Turtle):
+    """a ball for pong game that start moving randomlly, inherited from Turtle()"""
+    def __init__(self):
+        super().__init__()
+        

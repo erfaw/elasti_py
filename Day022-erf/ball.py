e2515c6189ff06e06f7f22335f9754077ba2e27b
erfaw <erfawn.h@gmail.com>
2025-02-16 22:45:04 +0330

    rename 'prepare_appearance()' to '__prepare_appearance()' , 2.yekari kardim bere yejaye random vayse baraye bare aval ke initialize mishe

--- Day022-erf/ball.py
+++ Day022-erf/ball.py
@@ -1,7 +1,18 @@
 from turtle import Turtle
-
+import random
+from screen import DOMAIN_Y
 class Ball(Turtle):
     """a ball for pong game that start moving randomlly, inherited from Turtle()"""
     def __init__(self):
         super().__init__()
+        self.__prepare_appearance()
         
+    def __prepare_appearance(self):
+        self.shape('circle')
+        self.color('red')
+        self.penup()
+        self.teleport(
+            x=-5,
+            y= random.randint
+            (-int(DOMAIN_Y),+int(DOMAIN_Y))
+            )

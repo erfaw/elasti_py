c805547dea2a1f56c351eeee89436b527181288c
erfaw <erfawn.h@gmail.com>
2025-02-16 21:07:39 +0330

    ye parameter be __init__() ezafe shd be name 'position' ke bayad bege paddle kodom taraf sakhte beshe. ValueError() gozashtim vase else ke right or left nabod. code haye marbot be 'right' va 'left' ro zadim.

--- Day022-erf/paddle.py
+++ Day022-erf/paddle.py
@@ -1,10 +1,25 @@
 from turtle import Turtle
+import screen
 class Paddle(Turtle):
-    def __init__(self):
+    def __init__(self, position):
         super().__init__()
+        self.position = position.lower()
         self.make_paddle()
         
     def make_paddle(self):
         self.color('white')
         self.shape('square')
         self.turtlesize(stretch_len=0.7, stretch_wid=3)
+        
+        if self.position == "left":
+            self.teleport(
+                -(screen.WIDTH/2) + 20 ,
+                0
+            )
+        elif self.position == "right":
+            self.teleport(
+               - (-(screen.WIDTH/2) + 25),
+                0
+            )
+        else:
+            raise ValueError("incorrect side, only 'right' and 'left' ")

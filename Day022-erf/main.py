a220dd7fe7d81140d530debcc9c49e40f67e64be
erfaw <erfawn.h@gmail.com>
2025-02-16 22:10:37 +0330

    event listener ha baraye key haye 'w', 's' (be manzore right_paddle) va 'upArrow' va 'downArrow' (be manzore left_paddle) ezafe shdnd. 2.ye while kolli baraye test kardane event listener ha ezafe shd.

--- Day022-erf/main.py
+++ Day022-erf/main.py
@@ -1,8 +1,27 @@
 from screen import my_sc, white_box
 from paddle import Paddle
+import time
 
 left_paddle = Paddle(position="left")
 right_paddle = Paddle(position="right")
 
-my_sc.update()
+# add event listeners
+my_sc.onkey(
+    left_paddle.move_up
+    ,'w')
+my_sc.onkey(
+    left_paddle.move_down
+    ,'s')
+my_sc.onkey(
+    right_paddle.move_up
+    ,'Up')
+my_sc.onkey(
+    right_paddle.move_down
+    ,'Down')
+my_sc.listen()
+
+while True:
+    time.sleep(0.1)
+    my_sc.update()
+    
 my_sc.exitonclick()

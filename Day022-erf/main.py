9fe3757bd89cf7318d657aa52fbb81bfce31916c
erfaw <erfawn.h@gmail.com>
2025-02-27 00:13:09 +0330

    dar talash baraye drst kardan reflect_paddle (left_paddle):: 1. tashkhis bar paye xcor() drst anjam mishe. | 2. toye barkhorde mostaghim va bedone zaive ke mishe heading()=180 drst kar mikne. | 3. andaze drst ke bashe +20 be xcor() dar elif ezafe shd ke daghighan az roye paddle reflect kne.

--- Day022-erf/main.py
+++ Day022-erf/main.py
@@ -22,14 +22,21 @@ my_sc.onkey(
     right_paddle.move_down
     ,'Down')
 my_sc.listen()
-ball.setheading(75)
+ball.sety(0)
+ball.setheading(180)
 while game_over == False:
     #tayin reflect baraye divar ha
     if (ball.ycor() >= DOMAIN_Y-20 or ball.ycor() <= -(DOMAIN_Y-20) ) and ball.is_stop == True:
         ball.reflect_wall()
     # tayin reflect paddle
-    else:
-        ball.move()
+    elif left_paddle.xcor() < ball.xcor() <= left_paddle.xcor()+20 :
+        if left_paddle.ycor()-3 < ball.ycor() < left_paddle.ycor()+3 :
+            ball.reflect_paddle()
+
+    # elif left_paddle.distance(ball) <= 20:
+    #     ball.reflect_paddle()
+        
+    ball.move()
 
         
     time.sleep(0.1)

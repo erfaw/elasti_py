7cfc9ed80667735a294c2c06f817d072a85c1d37
erfaw <erfawn.h@gmail.com>
2025-03-02 23:47:40 +0330

    number baraye samte rast ba moshakhasat va andaze o position e drst sakhte shd.

--- Day022-erf/scoreboard.py
+++ Day022-erf/scoreboard.py
@@ -1,6 +1,17 @@
 from turtle import Turtle
-class Scoreboard(Turtle):
+from screen import DOMAIN_X, DOMAIN_Y
+NUMBER_SIZE = 60
+FONT = "Arial"
+class Scoreboard:
     def __init__(self):
-        super().__init__()
-
+        self.right_score = Turtle()
+        self.left_score = Turtle()
+        self.make_scoreboard()
     
+    def make_scoreboard(self):
+        """making scoreboard style and first initialized"""
+        self.right_score.hideturtle()
+        # self.left_score.hideturtle()
+        self.right_score.color('white')
+        self.right_score.teleport(70,DOMAIN_Y-120)
+        self.right_score.write(0,False, 'center', (FONT, NUMBER_SIZE, "bold"))

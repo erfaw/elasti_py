30f26040a60f1314f61a1924f457d6246fc27a61
erfaw <erfawn.h@gmail.com>
2025-03-04 23:07:15 +0330

    1. variable 'game_over' mojadad ezafe shd. | 2. 2ta function be nam haye 'on_enter_pressed' va 'on_escape_pressed' baraye buffer kardane press shnde in 2 key ezafe shd. | 3. event listener baraye 2 key 'Escape' va 'Enter' ezafe shd (be manzore edame bazi va ya laghve bazi) | 4. function 'game_round' sakhte shd ke shamel 1round bazie. | 5. ye while baraye repeatable shdn bazi ezafe kardim va dakhelesh ba call karan 'game_round()' va estefade az variable haye sakhte shde be name 'enter_pressed' va 'escape_pressed' ravand bazi ro control miknim. | 6. my_sc.exitonmyclick() remove shd.

--- Day022-erf/main.py
+++ Day022-erf/main.py
@@ -9,7 +9,18 @@ right_paddle = Paddle(position="right")
 ball = Ball()
 scoreboard = Scoreboard()
 round_over = False
+game_over = False
+enter_pressed = False
+escape_pressed = False
 
+def on_enter_pressed():
+    global enter_pressed
+    if not round_over:
+        enter_pressed = True
+def on_escape_pressed():
+    global escape_pressed
+    if not round_over:
+        escape_pressed = True
 # add event listeners
 my_sc.onkey(
     left_paddle.move_up
@@ -23,11 +34,16 @@ my_sc.onkey(
 my_sc.onkey(
     right_paddle.move_down
     ,'Down')
+my_sc.onkey(
+    on_enter_pressed
+    ,'Return') # 'enter' key on keyboard
+my_sc.onkey(
+    on_escape_pressed
+    ,'Escape') # 'enter' key on keyboard
 my_sc.listen()
 
-# Right paddle x_cor = 475
-# Left paddle x_cor = -480
-while True:
+def game_round():
+    my_sc.update()
     round_over = False
     ball.first_place()
     while not round_over:
@@ -43,15 +59,24 @@ while True:
                 print('score for right')
                 scoreboard.score_for_right()
                 round_over = True
+                
             elif ball.xcor() > right_paddle.xcor() and ball.is_stop:
                 print('score for left')
                 scoreboard.score_for_left()
                 round_over = True
-
-        ball.move()
-
             
+        ball.move()
         time.sleep(0.1)
         my_sc.update()
-    
-my_sc.exitonclick()
+
+my_sc.update()
+while not game_over:
+    if enter_pressed:
+        game_round()
+        enter_pressed = False
+    elif escape_pressed:
+        game_over = True
+    else: 
+        pass
+    time.sleep(0.1)
+    my_sc.update()

10c9fffd155eaa20ff26804c58fa70948b05ac51
erfaw <erfawn.h@gmail.com>
2025-03-04 21:03:58 +0330

    adade samte chap ezafe shd. name turtle haye score number ha be 'left' va 'right' taghir kard. (prev:: 'left_score', 'right_score')

--- Day022-erf/scoreboard.py
+++ Day022-erf/scoreboard.py
@@ -4,14 +4,20 @@ NUMBER_SIZE = 60
 FONT = "Arial"
 class Scoreboard:
     def __init__(self):
-        self.right_score = Turtle()
-        self.left_score = Turtle()
+        self.right = Turtle()
+        self.left = Turtle()
         self.make_scoreboard()
     
     def make_scoreboard(self):
         """making scoreboard style and first initialized"""
-        self.right_score.hideturtle()
-        # self.left_score.hideturtle()
-        self.right_score.color('white')
-        self.right_score.teleport(70,DOMAIN_Y-120)
-        self.right_score.write(0,False, 'center', (FONT, NUMBER_SIZE, "bold"))
+        # prepare right
+        self.right.hideturtle()
+        self.right.color('white')
+        self.right.teleport(70,DOMAIN_Y-120)
+        self.right.write(0,False, 'center', (FONT, NUMBER_SIZE, "bold"))
+
+        # prepare left
+        self.left.hideturtle()
+        self.left.color('white')
+        self.left.teleport(70,DOMAIN_Y-120)
+        self.left.write(0,False, 'center', (FONT, NUMBER_SIZE, "bold"))

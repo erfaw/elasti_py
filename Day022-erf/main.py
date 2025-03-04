c64f1f914021e86c8f5a12cc9792b10df86433ce
erfaw <erfawn.h@gmail.com>
2025-03-04 21:47:26 +0330

    1. estefadeye variable 'game_over' ro taghir ddim va ona ro be 'round_over' ddim baraye inke betonim bazi ro repeatable knim. | 2.ye halghe while sakhtim ke repeatable beshe, break ndrim barahs dar hale hazer.

--- Day022-erf/main.py
+++ Day022-erf/main.py
@@ -8,7 +8,7 @@ left_paddle = Paddle(position="left")
 right_paddle = Paddle(position="right")
 ball = Ball()
 scoreboard = Scoreboard()
-game_over = False
+round_over = False
 
 # add event listeners
 my_sc.onkey(
@@ -27,28 +27,31 @@ my_sc.listen()
 
 # Right paddle x_cor = 475
 # Left paddle x_cor = -480
-while game_over == False:
-    #tayin reflect baraye divar ha
-    if ball.is_near_wall() and ball.is_stop == True:
-        ball.reflect_wall()
-    # tayin reflect paddle
-    elif right_paddle.is_hit_ball(ball.pos()) or left_paddle.is_hit_ball(ball.pos()) :
-        ball.reflect_paddle()
-    # tayin game over va scoring
-    elif not left_paddle.xcor() <= ball.xcor() <= right_paddle.xcor():
-        if ball.xcor() < left_paddle.xcor() and ball.is_stop:
-            print('score for right')
-            scoreboard.score_for_right()
-            game_over = True
-        elif ball.xcor() > right_paddle.xcor() and ball.is_stop:
-            print('score for left')
-            scoreboard.score_for_left()
-            game_over = True
+while True:
+    round_over = False
+    ball.first_place()
+    while not round_over:
+        #tayin reflect baraye divar ha
+        if ball.is_near_wall() and ball.is_stop == True:
+            ball.reflect_wall()
+        # tayin reflect paddle
+        elif right_paddle.is_hit_ball(ball.pos()) or left_paddle.is_hit_ball(ball.pos()) :
+            ball.reflect_paddle()
+        # tayin game over va scoring
+        elif not left_paddle.xcor() <= ball.xcor() <= right_paddle.xcor():
+            if ball.xcor() < left_paddle.xcor() and ball.is_stop:
+                print('score for right')
+                scoreboard.score_for_right()
+                round_over = True
+            elif ball.xcor() > right_paddle.xcor() and ball.is_stop:
+                print('score for left')
+                scoreboard.score_for_left()
+                round_over = True
 
-    ball.move()
+        ball.move()
 
-        
-    time.sleep(0.1)
-    my_sc.update()
+            
+        time.sleep(0.1)
+        my_sc.update()
     
 my_sc.exitonclick()

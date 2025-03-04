ba7eb0ce7e94f3f823b8d104e4795926a0eb7f9a
erfaw <erfawn.h@gmail.com>
2025-03-04 21:25:15 +0330

    1. variable 'self.right_score' va 'self.left_score' ezafe shd. | 2.method haye 'score_for_left()' va 'score_for_right()' ezafe shd.

--- Day022-erf/scoreboard.py
+++ Day022-erf/scoreboard.py
@@ -6,6 +6,8 @@ class Scoreboard:
     def __init__(self):
         self.right = Turtle()
         self.left = Turtle()
+        self.right_score = 0
+        self.left_score = 0
         self.make_scoreboard()
     
     def make_scoreboard(self):
@@ -14,10 +16,42 @@ class Scoreboard:
         self.right.hideturtle()
         self.right.color('white')
         self.right.teleport(70,DOMAIN_Y-120)
-        self.right.write(0,False, 'center', (FONT, NUMBER_SIZE, "bold"))
+        self.right.write(
+            self.right_score,
+            False,
+            'center',
+            (FONT, NUMBER_SIZE, "bold")
+            )
 
         # prepare left
         self.left.hideturtle()
         self.left.color('white')
         self.left.teleport(-75,DOMAIN_Y-120)
-        self.left.write(0,False, 'center', (FONT, NUMBER_SIZE, "bold"))
+        self.left.write(
+            self.left_score,
+            False,
+            'center',
+            (FONT, NUMBER_SIZE, "bold")
+            )
+
+    def score_for_right(self):
+        """give score to right and update it in screen"""
+        self.right_score += 1
+        self.right.clear()
+        self.right.write(
+        self.right_score,
+        False,
+        'center',
+        (FONT, NUMBER_SIZE, "bold")
+        )
+
+    def score_for_left(self):
+        """give score to left and update it in screen"""
+        self.left_score += 1
+        self.left.clear()
+        self.left.write(
+        self.left_score,
+        False,
+        'center',
+        (FONT, NUMBER_SIZE, "bold")
+        )

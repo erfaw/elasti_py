724106bfe5295e4561ef9f3efc960c3c84631c54
erfaw <erfawn.h@gmail.com>
2025-03-05 22:36:13 +0330

    hameye aspect haye marbot be screen.py OOP shodan.

--- Day022-erf/screen.py
+++ Day022-erf/screen.py
@@ -1,74 +1,88 @@
 from turtle import Screen, Turtle
-# it's so important to set widht and height from here.
 WIDTH = 1000
 HEIGHT = 650
 DOMAIN_X = WIDTH/2
 DOMAIN_Y = HEIGHT/2
-my_sc = Screen()
-my_sc.tracer(0)
-my_sc.setup(width=WIDTH, height=HEIGHT+50)
-my_sc.bgcolor('black')
 
-# baraye jelogiri az change size window e bazi , toplevel ro dakhele ye variable mirizim o baad method 'resizable()' ro call  miknim va width o height ro False gharar midim.
-root_window = my_sc.getcanvas().winfo_toplevel()
-root_window.resizable(False, False)
+class Sc:
+    def __init__(self):
+        self.my_sc = Screen()
+        self.root_window = self.my_sc.getcanvas().winfo_toplevel()
+        self.mid_line = Turtle()
+        self.white_box = Turtle()
+        self.details = Turtle()
+        self.delay_notif = Turtle()
+        self.__prepare_screen()
+        self.window_size_lock()
+        self.__prepare_mid_line()
+        self.__prepare_white_box()
+        self.__prepare_details()
+        self.__prepare_delay_notif()
+        self.my_sc.update()
 
-mid_line = Turtle()
-mid_line.teleport(-5, 325)
-mid_line.hideturtle()
-mid_line.pencolor('white')
-mid_line.setheading(270)
-mid_line.pensize(5)
 
-while mid_line.ycor() > -325:
-    mid_line.fd(10)
-    mid_line.penup()
-    mid_line.fd(10)
-    mid_line.pendown()
+    def __prepare_screen(self):
+        self.my_sc.tracer(0)
+        self.my_sc.setup(width=WIDTH, height=HEIGHT+50)
+        self.my_sc.bgcolor('black')
+
+    def window_size_lock(self):
+        self.root_window.resizable(False, False)
+
+    def window_size_unlock(self):
+        self.root_window.resizable(True, True)
+
+    def __prepare_mid_line(self):
+        self.mid_line.teleport(-5, 325)
+        self.mid_line.hideturtle()
+        self.mid_line.pencolor('white')
+        self.mid_line.setheading(270)
+        self.mid_line.pensize(5)
+        while self.mid_line.ycor() > -325:
+            self.mid_line.fd(10)
+            self.mid_line.penup()
+            self.mid_line.fd(10)
+            self.mid_line.pendown()
+
+    def __prepare_white_box(self):
+        self.white_box.hideturtle()
+        self.white_box.pencolor('white')
+        self.white_box.pensize(3)
+        self.white_box.teleport(
+            -DOMAIN_X,+DOMAIN_Y
+        )
+        self.white_box.goto(
+            +DOMAIN_X, +DOMAIN_Y
+        )
+        self.white_box.goto(
+            +DOMAIN_X, -DOMAIN_Y
+        )
+        self.white_box.goto(
+            -DOMAIN_X, -DOMAIN_Y
+        )
+        self.white_box.goto(
+            -DOMAIN_X, +DOMAIN_Y
+        )
     
-white_box = Turtle()
-white_box.hideturtle()
-white_box.pencolor('white')
-white_box.pensize(3)
-white_box.teleport(
-    -DOMAIN_X,+DOMAIN_Y
-)
-white_box.goto(
-    +DOMAIN_X, +DOMAIN_Y
-)
-white_box.goto(
-    +DOMAIN_X, -DOMAIN_Y
-)
-white_box.goto(
-    -DOMAIN_X, -DOMAIN_Y
-)
-white_box.goto(
-    -DOMAIN_X, +DOMAIN_Y
-)
-my_sc.update()
+    def __prepare_details(self):
+        self.details.hideturtle()
+        self.details.pencolor('white')
+        self.details.teleport(-DOMAIN_X+10, DOMAIN_Y)
+        self.details.write(
+            "Up=W    Down=S",
+            False,
+            "left",
+            ("Arial", 12, "bold")
+            )
+        self.details.teleport(-(-DOMAIN_X+10), DOMAIN_Y)
+        self.details.write(
+            "Up=↑    Down=↓",
+            False,
+            "right",
+            ("Arial", 12, "bold")
+        )
 
-details = Turtle()
-# details.penup()
-details.hideturtle()
-details.pencolor('white')
-details.teleport(-DOMAIN_X+10, DOMAIN_Y)
-details.write(
-    "Up=W    Down=S",
-    False,
-    "left",
-    ("Arial", 12, "bold")
-    )
-details.teleport(-(-DOMAIN_X+10), DOMAIN_Y)
-details.write(
-    "Up=↑    Down=↓",
-    False,
-    "right",
-    ("Arial", 12, "bold")
-)
-
-delay_notif = Turtle()
-delay_notif.hideturtle()
-delay_notif.teleport(-15,0)
-delay_notif.pencolor('white')
-
-my_sc.update()
+    def __prepare_delay_notif(self):
+        self.delay_notif.hideturtle()
+        self.delay_notif.teleport(-15,0)
+        self.delay_notif.pencolor('white')

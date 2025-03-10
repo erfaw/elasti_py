6a38732de8ad4831c9b7787c66808df674d70a12
erfaw <erfawn.h@gmail.com>
2025-03-10 22:21:34 +0330

    class Player sakhte shd va ba '__prepare()' maghadir avalie radif shd, methode 'move_up()' ezafe shd

--- Day023-erf/002 turtle-crossing-start/player.py
+++ Day023-erf/002 turtle-crossing-start/player.py
@@ -1,7 +1,20 @@
+from turtle import Turtle
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.__prepare()
+
+    def __prepare(self):
+        self.penup()
+        self.shape('turtle')
+        self.teleport(STARTING_POSITION[0], STARTING_POSITION[1])
+        self.setheading(90)
+
+    def move_up(self):
+        self.fd(MOVE_DISTANCE)
+

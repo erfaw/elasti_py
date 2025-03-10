1409155b93576103a9d4c42ed922c4f81d699848
erfaw <erfawn.h@gmail.com>
2025-03-10 22:39:31 +0330

    nemone avalie car sakhte shd ba color random. faghat 1 car

--- Day023-erf/002 turtle-crossing-start/car_manager.py
+++ Day023-erf/002 turtle-crossing-start/car_manager.py
@@ -1,7 +1,16 @@
+from turtle import Turtle
+import random
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
 class CarManager:
-    pass
+    def __init__(self):
+        self.make_car()
+
+    def make_car(self):
+        new_car = Turtle()
+        new_car.shape('square')
+        new_car.shapesize(stretch_len=2 , stretch_wid=1)
+        new_car.color(random.choice(COLORS))

099a90b0154c3b0165467398660f569aa338b62b
erfaw <erfawn.h@gmail.com>
2025-03-10 22:22:55 +0330

    object 'player' az class Player sakhte shd. va event listener o 'listen()' baraye dokmeye 'Up' ezafe shd.

--- Day023-erf/002 turtle-crossing-start/main.py
+++ Day023-erf/002 turtle-crossing-start/main.py
@@ -7,7 +7,9 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
-
+player = Player()
+screen.onkey(key="Up", fun= player.move_up)
+screen.listen()
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)

46ecdfedaf15c2081beed1c361be997a879875dd
erfaw <erfawn.h@gmail.com>
2025-03-16 22:46:28 +0330

    1. varibale 'MOVE_INCREMENT' az file 'car_manager.py' import shd, | 2. background color ro be 'gray' taghir ddim ta cheshm ro aziat nakone. | 3. variable 'i' baraye shomarande tedad anjam halghe while ezafe shd. | 4. har 6 bar yekbar 'cars.make_car()' ejra shd. | 5. az oon car hayi ke az tasvir kharej shode bodan baraye memory management az dobare estefade kardim (toye list cars.reuse_list buffer mishan va az cars.list remove mishan. | 6. harekat be andazeye 'MOVE_INCREMENT' be sorate daemi baraye car in cars.list, | 7. har marhale az loop while i+=1 mishe.

--- Day023-erf/002 turtle-crossing-start/main.py
+++ Day023-erf/002 turtle-crossing-start/main.py
@@ -1,11 +1,12 @@
 import time
 from turtle import Screen
 from player import Player
-from car_manager import CarManager
+from car_manager import CarManager, MOVE_INCREMENT
 from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
+screen.bgcolor('gray')
 screen.tracer(0)
 player = Player()
 screen.onkey(key="Up", fun= player.move_up)
@@ -15,6 +16,16 @@ cars = CarManager()
 
 
 game_is_on = True
+i = 1
 while game_is_on:
+    if i % 6 == 0:
+        cars.make_car()
+        for car in cars.list:
+            if car.xcor() < -310:
+                cars.reuse_list.append(car)
+                cars.list.remove(car)
+    for car in cars.list:
+        car.fd(MOVE_INCREMENT)
     time.sleep(0.1)
     screen.update()
+    i += 1

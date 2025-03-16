4fbdb23379b3d9a108911b77ac635a970ce84384
erfaw <erfawn.h@gmail.com>
2025-03-16 22:34:37 +0330

    1. attribute 'list' va 'reuse_list' sakhte shd. | 2. method 'make_car()' sakhte shd. barash 2 halat gharar ddim ke agar reuse list khalie 'new_car' besaz append kn be 'list' va agar 'reuse_list' khali nabod, avalin elemente oon ro teleport kn jayi ke bayad bashe va append kn be 'list' va az 'reuse_list' pakesh kn. | 3. 'reuse_list' hm intori por mishe ke car ke az -300 xcor() manfi tari peyda mikne be 'reuse_list' ezafe mishe va az 'list' remove mishe. (be dalil estefadeye mojadad az car haye sakhte shde, chera ke agar bekhad ta abad object besaze baad az chand marhale kheyli sangin mishe, dar hale hazer memory usage mire ta 20.7 MB va balatar nmire .

--- Day023-erf/002 turtle-crossing-start/car_manager.py
+++ Day023-erf/002 turtle-crossing-start/car_manager.py
@@ -7,10 +7,30 @@ MOVE_INCREMENT = 10
 
 class CarManager:
     def __init__(self):
-        self.make_car()
+        self.list = []
+        self.reuse_list = []
 
     def make_car(self):
-        new_car = Turtle()
-        new_car.shape('square')
-        new_car.shapesize(stretch_len=2 , stretch_wid=1)
-        new_car.color(random.choice(COLORS))
+        if self.reuse_list == []:
+            new_car = Turtle()
+            new_car.shape('square')
+            new_car.shapesize(stretch_len=2 , stretch_wid=1)
+            new_car.color(random.choice(COLORS))
+            new_car.penup()
+            new_car.seth(180)
+            new_car.teleport(
+                300,
+                random.randint(-250, 250)
+                )
+            self.list.append(new_car)
+        else:
+            self.list.append(
+                self.reuse_list[0]
+            )
+            self.list[-1].teleport(
+                300,
+                random.randint(-250, 250)
+                )
+            self.reuse_list.remove(
+                self.reuse_list[0]
+            )

04147d6a89cbed3cd7709905a00d47e6c18d266f
erfaw <erfawn.h@gmail.com>
2025-04-04 23:19:59 +0330

    class Scoreboard be sorate kamel sakhte shd.

--- Day023-erf/002 turtle-crossing-start/scoreboard.py
+++ Day023-erf/002 turtle-crossing-start/scoreboard.py
@@ -1,5 +1,24 @@
+from turtle import Turtle
 FONT = ("Courier", 24, "normal")
 
 
 class Scoreboard:
-    pass
+    def __init__(self):
+        self.level = 1
+        self.__prepare_writer()
+
+    def __prepare_writer(self):
+        self.writer = Turtle()
+        self.writer.penup()
+        self.writer.hideturtle()
+        self.writer.teleport(-295, 255)
+        self.writer.write(f"Level: {self.level}", False, "left", FONT)
+
+    def update_level(self):
+        self.level += 1
+        self.writer.clear()
+        self.writer.write(f"Level: {self.level}", False, "left", FONT)
+
+    def game_over(self):
+        self.writer.teleport(-100,0)
+        self.writer.write("Game Over!!", False, "left", FONT)

ac01e1a4f26b0ccbb3a9b6fda63a3a4f161e56cb
erfaw <erfawn.h@gmail.com>
2025-04-04 22:34:16 +0330

    comment hayei ke baraye test bodan ro az comment dar ovordim. (not important)

--- Day023-erf/002 turtle-crossing-start/main.py
+++ Day023-erf/002 turtle-crossing-start/main.py
@@ -21,10 +21,10 @@ while game_is_on:
     if player.ycor() > FINISH_LINE_Y:
         player.teleport(STARTING_POSITION[0],STARTING_POSITION[1])
         MOVE_INCREMENT *= 1.2
-    # for car in cars.list:
-    #     if car.distance(player) <= 45 and car.ycor()-14 <= player.ycor() <= car.ycor()+14:
-    #         game_is_on = False
-    #         break
+    for car in cars.list:
+        if car.distance(player) <= 45 and car.ycor()-14 <= player.ycor() <= car.ycor()+14:
+            game_is_on = False
+            break
     if i % 6 == 0:
         cars.make_car()
         for car in cars.list:

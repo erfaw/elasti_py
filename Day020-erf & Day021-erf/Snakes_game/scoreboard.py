2b73a6328698e32db41e8bc50ec17ae33752e648
erfaw <146765011+erfaw@users.noreply.github.com>
2025-06-22 23:07:29 +0330

    ON 'scoreboard.py': 1. dar __init__() self.highest_score=0 ezafe shd, 2. dar 'make_scoreboard()' & 'update()' ghesmate neveshtane score ha update shd ke highest score ham neshon bede, 3. 'chec_highest_score()' ezafe shd ke highest score ro dar sorate bishtar bodan score por kne.

--- Day020-erf & Day021-erf/Snakes_game/scoreboard.py
+++ Day020-erf & Day021-erf/Snakes_game/scoreboard.py
@@ -3,6 +3,7 @@ from turtle import Turtle
 class Scoreboard:
     def __init__(self):
         self.score = 0
+        self.highest_score = 0
         self.my_board = self.make_scoreboard()
     
     def make_scoreboard(self): 
@@ -12,9 +13,16 @@ class Scoreboard:
         new_board.hideturtle()
         new_board.color("white")
         new_board.pendown()
-        new_board.write(f"Score: {self.score}", False, "center", ("Arial", 14, "normal"))
+        new_board.write(f"Score: {self.score},\t Highest Score: {self.highest_score}", False, "center", ("Arial", 14, "normal"))
         return new_board
     
     def update(self):
         self.my_board.clear()
-        self.my_board.write(f"Score: {self.score}", False, "center", ("Arial", 14, "normal"))
+        self.my_board.write(f"Score: {self.score},\t Highest Score: {self.highest_score}", False, "center", ("Arial", 14, "normal"))
+
+    def check_highest_score(self):
+        if self.score > self.highest_score:
+            self.highest_score = self.score
+            self.score = 0
+        else: 
+            self.score = 0

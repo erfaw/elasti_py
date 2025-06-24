c99ea57a69dded6a4dac10d9f94fa15f096c57e4
erfaw <146765011+erfaw@users.noreply.github.com>
2025-06-24 04:27:11 +0330

    ON 'main.py': 1.code haye marbot be 'cls' raftan to 1line, 2.az module 'loading_files.py' class 'LoadInput()' import shd toye variable 'load', 3.varibale haye 'names' & 'basic_letter' be komake 'LoadInput()' por shdn, va check shdn.

--- Day024-erf/Mail Merge Project Start/main.py
+++ Day024-erf/Mail Merge Project Start/main.py
@@ -2,22 +2,14 @@
 #for each name in invited_names.txt
 #Replace the [name] placeholder with the actual name.
 #Save the letters in the folder "ReadyToSend".
-import os
-os.system('cls')   
+import os; os.system('cls')
+from loading_files import LoadInput
 
-# open invited_names.txt for catch names in a list first
-with open("./Input/Names/invited_names.txt", mode='r') as f:
-    names = f.readlines()
+load = LoadInput()
 
-i=0
-for name in names:
-    names[i] = name.replace('\n', '')
-    i += 1
-
-with open("./Input/Letters/starting_letter.txt", mode='r') as f:
-    basic_letter = f.read()
+names = load.names()
+basic_letter = load.basic_letter()
 
 basic_letter = basic_letter.replace('[name]', 'erfan')
-
-
 print(basic_letter)
+print(names)

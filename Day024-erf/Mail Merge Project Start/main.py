e6cc10a5e4effa55c91b25e308462bc404370313
erfaw <146765011+erfaw@users.noreply.github.com>
2025-06-24 05:53:46 +0330

    Main Merge Project DONE. output files maded successfully.

--- Day024-erf/Mail Merge Project Start/main.py
+++ Day024-erf/Mail Merge Project Start/main.py
@@ -1,15 +1,11 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
 import os; os.system('cls')
 from loading_files import LoadInput
 
 load = LoadInput()
-
 names = load.names()
 basic_letter = load.basic_letter()
 
-basic_letter = basic_letter.replace('[name]', 'erfan')
-print(basic_letter)
-print(names)
+for name in names:
+    output_letter = basic_letter.replace('[name]', name)
+    with open(f"./Output/ReadyToSend/{name}.txt", mode='w') as f:
+        f.write(output_letter)

18c5bd8482d620e0d634f90d70c4b0a86f7e2c2f
erfaw <146765011+erfaw@users.noreply.github.com>
2025-06-24 04:09:02 +0330

    ON 'main.py' : 1.cls ezafe kardim, 2.path haro Relative kardim, 3.'basic_letter' ro sakhtim ke mishe ye name ro ba [name] jabeja kard.

--- Day024-erf/Mail Merge Project Start/main.py
+++ Day024-erf/Mail Merge Project Start/main.py
@@ -2,10 +2,11 @@
 #for each name in invited_names.txt
 #Replace the [name] placeholder with the actual name.
 #Save the letters in the folder "ReadyToSend".
-    
+import os
+os.system('cls')   
 
 # open invited_names.txt for catch names in a list first
-with open("g:/myDocuments/Programming/Python/elasti_py/Day024-erf/Mail Merge Project Start/Input/Names/invited_names.txt", mode='r') as f:
+with open("./Input/Names/invited_names.txt", mode='r') as f:
     names = f.readlines()
 
 i=0
@@ -13,3 +14,10 @@ for name in names:
     names[i] = name.replace('\n', '')
     i += 1
 
+with open("./Input/Letters/starting_letter.txt", mode='r') as f:
+    basic_letter = f.read()
+
+basic_letter = basic_letter.replace('[name]', 'erfan')
+
+
+print(basic_letter)

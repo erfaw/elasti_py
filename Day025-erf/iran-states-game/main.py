f612491758a7bbb24cbeb714965546843d88d242
erfaw <146765011+erfaw@users.noreply.github.com>
2025-09-12 11:32:19 +0330

    1. ba comment Title bandi kardim, 2. textinput sakhtim input ro rikhtim to answer_state, 3. data name o coordinate ostan ha ro ovordim roye variable 'state_data' va unnamed column ro nadare, 4. function 'process_user_guess()' ro baraye anjam farayand roye entekhabe user sakhtim loop zadim baraye barabar bodane javab if-else gozashtim (nakamele alan) moshkel khordim ba character farsi ke tashkhis dade nemishe va niaz be Normalize kardan dare.

--- Day025-erf/iran-states-game/main.py
+++ Day025-erf/iran-states-game/main.py
@@ -1,8 +1,31 @@
 import turtle, os; os.system('cls');
+import pandas as pd
 image = "./Day025-erf/iran-states-game/bg_resized.gif"
+
+#prepare screen and background image
 sc = turtle.Screen()
 sc.title("Iran States Game")
 sc.addshape(image)
 turtle.shape(image)
+
+#get guess from user
+answer_state = sc.textinput("حدس بزنید...",prompt="Enter Persian name of the IRAN states | نام فارسی استان های ایران را وارد کنید")
+# print (answer_state)
+
+states_data = pd.read_csv("./Day025-erf/iran-states-game/31_states.csv").loc[:, ["state", "xcor", "ycor"]]
+
+corrects = 0
+def process_user_guess():
+    for sn in states_data["state"]:
+        print(sn == answer_state)
+        if answer_state == sn:
+            print(f"{sn} was correct, GZ")
+            # 1. show name state on map
+            # 2. append to a list named {right list}
+            # 3. increase number of correct states
+        else:
+            pass
+
+process_user_guess()
 
 sc.exitonclick()

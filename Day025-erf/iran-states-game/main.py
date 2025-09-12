836b611f9320a48767174ecd02c7464393ac23e9
erfaw <146765011+erfaw@users.noreply.github.com>
2025-09-12 12:05:32 +0330

    1. while sakhtim baraye loop kardan barname, variable 'answer_state' ro montaghel kardim dakhelesh, 2. baraye inke hame name ostan haro check knim cls o 'process_user_guess()' ro toye loop call  kardim, 3. function 'normalize_fa_ar()' ro neveshtim ke char haye arabi ro ba farsi replace mikne, 4. print i ke dakhele 'process_user_guess()' bod remove shd va print i ke ebtedaye if bod edit shd, 5. az 'normalize_fa_ar()' roye output i ke az 'sc.textinput()' migereftim estefade kardim ke output correct bede.

--- Day025-erf/iran-states-game/main.py
+++ Day025-erf/iran-states-game/main.py
@@ -8,24 +8,31 @@ sc.title("Iran States Game")
 sc.addshape(image)
 turtle.shape(image)
 
-#get guess from user
-answer_state = sc.textinput("حدس بزنید...",prompt="Enter Persian name of the IRAN states | نام فارسی استان های ایران را وارد کنید")
-# print (answer_state)
-
 states_data = pd.read_csv("./Day025-erf/iran-states-game/31_states.csv").loc[:, ["state", "xcor", "ycor"]]
 
 corrects = 0
 def process_user_guess():
     for sn in states_data["state"]:
-        print(sn == answer_state)
         if answer_state == sn:
-            print(f"{sn} was correct, GZ")
+            print(f"GZ✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔")
             # 1. show name state on map
             # 2. append to a list named {right list}
             # 3. increase number of correct states
         else:
             pass
 
-process_user_guess()
+def normalize_fa_ar(text):
+    return (
+        text.replace("ي", "ی")  # عربی -> فارسی
+            .replace("ك", "ک")
+    )
+
+while True:
+    #get guess from user
+    answer_state = normalize_fa_ar(
+        sc.textinput("حدس بزنید...",prompt="Enter Persian name of the IRAN states | نام فارسی استان های ایران را وارد کنید")
+        )
+    os.system('cls')
+    process_user_guess()
 
 sc.exitonclick()

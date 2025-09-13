b01ab62eb4585ca2b4f576985d90468700cacda8
erfaw <146765011+erfaw@users.noreply.github.com>
2025-09-13 12:23:14 +0330

    1. class 'CorrectNameWriter' sakhte shd va azash object 'turtle_write' ro baryae neveshtane esme state ha sakhtim, 2. toye 'process_user_guess()' az 'turtle_write.on_map()' estefade kardim va esm ro print kardim dar ezaye correct guess, 3. 'exitonclick()' khat akhar ro remove kardim.

--- Day025-erf/iran-states-game/main.py
+++ Day025-erf/iran-states-game/main.py
@@ -7,15 +7,29 @@ sc = turtle.Screen()
 sc.title("Iran States Game")
 sc.addshape(image)
 turtle.shape(image)
-
 states_data = pd.read_csv("./Day025-erf/iran-states-game/31_states.csv").loc[:, ["state", "xcor", "ycor"]]
+
+class CorrectNameWriter(turtle.Turtle):
+    def __init__(self,):
+        super().__init__()
+        self.hideturtle()
+        self.penup()
+    def on_map(self, state_name):
+        self.goto(
+            int(states_data[states_data["state"] == state_name]["xcor"]),
+            int(states_data[states_data["state"] == state_name]["ycor"]),
+        )
+        self.write(f"{state_name}", False, "center", ("Arial", 12, "bold"))
+
+turtle_write = CorrectNameWriter()
+
 
 corrects = 0
 def process_user_guess():
     for sn in states_data["state"]:
         if answer_state == sn:
             print(f"GZ✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔")
-            # 1. show name state on map
+            turtle_write.on_map(answer_state)
             # 2. append to a list named {right list}
             # 3. increase number of correct states
         else:

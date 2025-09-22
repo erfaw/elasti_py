d55ebf44bbc4273a74952430e56dc0fe3f8c0a9c
erfaw <146765011+erfaw@users.noreply.github.com>
2025-09-22 10:33:12 +0330

    done the challenge :)

--- Day027-erf/main.py
+++ Day027-erf/main.py
@@ -7,7 +7,8 @@ window.title("shitSHOW :)")
 
 #make a label
 my_label = Label(text="its gonna be a lable", font=("Calibri", 25, "bold"))
-my_label.pack()
+my_label.grid(row=1,column=1)
+# my_label.pack()
 
 # button functionality: change my_label['text'] when clicked
 def button_clicked():
@@ -20,13 +21,23 @@ def button_clicked():
     
 # make a button
 my_button = Button(text="Click me Motha Fucka", command=button_clicked, pady=20)
-my_button.pack()
-my_label2 = Label(text="", font=("Calibri", 15, "bold"))
-my_label2.pack()
+my_button.grid(row=2,column=2)
+# my_button.pack()
+
+#make another label
+my_label3 = Label(text="its gonna be another lable", font=("Calibri", 25, "bold"))
+my_label3.grid(row=1,column=3)
+# my_label.pack()
+
+#label for put in some margin :)
+# my_label2 = Label(text="", font=("Calibri", 15, "bold"))
+
+# my_label2.pack()
 
 # using Entry module
 input1 = Entry(width=50)
-input1.pack()
+input1.grid(row=3,column=4)
+# input1.pack()
 
 
 

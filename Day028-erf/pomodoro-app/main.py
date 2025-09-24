99817699292bb7aa6acc99468c26254f5ff09a2f
erfaw <146765011+erfaw@users.noreply.github.com>
2025-09-24 11:10:16 +0330

    be manzore 'Reset' button yeseri kara anjam ddim ke functionality ro implement knim: 1. toye 'update_timer()' sare rahe create_text i ke timer ro grid mikard va nemayesh midad ye if-else gozashtim, ke boolean 'reset_but_click' ro check mikne va agar True bod 00:00 ro grid mikne va 'reset_but_click' ro False mikne, va agar 'reset-but_click' False bashe else run mishe ke baes mishe create_text time elapsed ro grid kne va nemayesh bede va intori timer be kare khodesh edame mide, 2. function 'reset_but_clicked()' ro declare kardim, ghablesh ham variable bool 'reset_but_click' ro False declare kardim, dakhele 'reset_but_clicked()' bool ro True kardim, 3. attribute 'command' ro baraye 'Reset' button =reset_but_clicked gozashtim.

--- Day028-erf/pomodoro-app/main.py
+++ Day028-erf/pomodoro-app/main.py
@@ -20,6 +20,7 @@ def start_timer():
     update_timer()
 
 def update_timer():
+    global reset_but_click
     elapsed = int(time.time() - start_time)
     formated = time.strftime("%M:%S", time.gmtime(elapsed))
     layer_1.create_image(
@@ -27,14 +28,24 @@ def update_timer():
     114,
     image=tomato_png
     )
-    layer_1.create_text(
+    if reset_but_click:
+        layer_1.create_text(
         102,128,
-        text=formated,
+        text="00:00",
         font=(FONT_NAME, 22, "bold"),
         fill="white"
-    )
-    layer_1.grid(row=1, column=1)
-    window.after(1000, update_timer)
+        )
+        layer_1.grid(row=1,column=1)
+        reset_but_click= False
+    else:
+        layer_1.create_text(
+            102,128,
+            text=formated,
+            font=(FONT_NAME, 22, "bold"),
+            fill="white"
+        )
+        layer_1.grid(row=1, column=1)
+        window.after(1000, update_timer)
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -86,11 +97,17 @@ start_but = Button(
     )
 start_but.grid(row=2,column=0)
 
+reset_but_click= False
+def reset_but_clicked():
+    global reset_but_click
+    reset_but_click = True
+
 reset_but = Button(
     text='Reset',
     bg='white',
     borderwidth=1,
-    font=("Arial", 8,"bold")
+    font=("Arial", 8,"bold"),
+    command=reset_but_clicked,
     )
 reset_but.grid(row=2,column=2)
 check_mark_char = '✅'

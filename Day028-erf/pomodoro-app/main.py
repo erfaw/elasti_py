f802cfd4712fd1c78fa7792ed0ff96407e8b6beb
erfaw <146765011+erfaw@users.noreply.github.com>
2025-09-25 12:18:53 +0330

    HUGE COMMINT in 'main.py': 1. import 'time' ro remove kardim, 2. az 'time_manager.py' class 'TimeManager()' ro import kardim va azash object sakhtim be name 'timer', 3. code main ro ba attribute ha va method haye 'timer' refactor kardim va check kardim kar mikne, 4. comment ha va code haye ghabli ro remove kardim.

--- Day028-erf/pomodoro-app/main.py
+++ Day028-erf/pomodoro-app/main.py
@@ -1,47 +1,30 @@
 from tkinter import *
-import time
 import subprocess as sp; sp.call('cls', shell=True)
 from CONSTANTS_variable import *
 from interface import *
-tomato_png_file_path = "./Day028-erf/pomodoro-app/tomato.png"
+from time_manager import TimeManager
 
-#make UI
 window = PomodoroWindow()
-reset_but_click= False
+timer = TimeManager()
+
 def reset_but_clicked():
-    global reset_but_click
-    reset_but_click = True
+    window.is_reset_clicked = True
+
 def update_timer():
-    global reset_but_click
-    elapsed = int(time.time() - start_time)
-    formated = time.strftime("%M:%S", time.gmtime(elapsed))
-    window.layer_1.create_image(
-    102,
-    114,
-    image=window._tomato()
-    )
-    if reset_but_click:
-        window.layer_1.create_text(
-        102,128,
-        text="00:00",
-        font=(FONT_NAME, 22, "bold"),
-        fill="white"
-        )
-        window.layer_1.grid(row=1,column=1)
-        reset_but_click= False
+    elapsed = timer.elapsed(timer.start)
+    formated = timer.format_time(elapsed)
+    window._tomato()
+    if window.is_reset_clicked:
+        window._raw_time_str()
+        window.is_reset_clicked = False
     else:
-        window.layer_1.create_text(
-            102,128,
-            text=formated,
-            font=(FONT_NAME, 22, "bold"),
-            fill="white"
-        )
-        window.layer_1.grid(row=1, column=1)
+        window.update_time_str(formated)
         window.after(1000, update_timer)
+
 def start_timer():
-    global start_time
-    start_time= time.time() #current moment
+    timer.start = timer.current()
     update_timer()
+
 window.make_start_but(start_timer)
 window.make_reset_but(reset_but_clicked)
 

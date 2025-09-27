dd09f633278ab57ea76e56a80e4e0278267da98f
erfaw <146765011+erfaw@users.noreply.github.com>
2025-09-27 12:08:44 +0330

    in 'main.py': 1. toye 'send_notification()' attribute 'timeout' ro =3 kardim (was =30), 2. toye 'update_timer()' local variable 'target_duration_s' ro =3 kardim baraye kare rahat tar (was =10), 3. on line 41: comment add shd baraye if, 4. on line 45: code ghabli ke string baraye 5min-break-period neveshte mishd remove shd, va be jash, attribute 'window.label_note_5min' config shd roye fg=RED, (was =YELLOW, ke nemayesh dade nashe yejorayi), 5. function 'update_timer_for_5minute()' ro declare kardim, az roye 'update_timer()' neveshtimesh baraye 5 min break ro neshon bede, roye maghadire testie fe'li check shd okaye, 6. function 'start_timer()' ba 'update_timer_for_5minute()' va farayandesh por shd, (need fix)

--- Day028-erf/pomodoro-app/main.py
+++ Day028-erf/pomodoro-app/main.py
@@ -13,7 +13,8 @@ def send_notification(message):
     notification.notify(
         title="Pomodoro App",
         message=message,
-        timeout=30  # Duration in seconds
+        timeout=3  # Duration in seconds
+        # timeout=30  # Duration in seconds
     )
 
 def reset_but_clicked():
@@ -21,7 +22,7 @@ def reset_but_clicked():
 
 def update_timer():
     global after_update_timer
-    target_duration_s = 10 # 25 minute
+    target_duration_s = 3 # 25 minute
     # target_duration_s = 25*60 # 25 minute
     break_time_s = 10 # 5 minute
     # break_time_s = 5*60 # 5 minute
@@ -37,15 +38,12 @@ def update_timer():
         window.after_cancel(after_update_timer)
     else:
         if timer.elapsed_s > target_duration_s:
+            # for achieving target time for one period
             window.pomodoro_round += 1
             send_notification("You must take a short break! (5min)")
             window._tomato()
-            window.layer_1.create_text(
-                102,128,
-                text="ON BREAK!!! (5min)\nbe Sharp for Notification",
-                fill="White",
-                font=(FONT_NAME, 25, "bold")
-            )
+            window.label_note_5min.config(fg=RED)
+            window.label_for_ticks()
             window.after_cancel(after_update_timer)
             # time.sleep(break_time_s)
             # send_notification("Your Break is DONE, get back to work")
@@ -55,13 +53,45 @@ def update_timer():
             window.label_for_ticks()
             after_update_timer = window.after(1000, update_timer)
 
+def update_timer_for_5minute():
+    global after_update_timer_5
+    target_duration_s = 10 # must be 5 minute
+    timer.elapsed_s = timer.elapsed(timer.start)
+    timer.formated_str = timer.format_time(timer.elapsed_s)
+    window._tomato()
+
+    if window.is_reset_clicked:
+        window._raw_time_str()
+        window.pomodoro_round = 0
+        window.label_tick.config(fg= YELLOW) #for remove it from display
+        window.is_reset_clicked = False
+        window.after_cancel(after_update_timer_5)
+    else:
+        if timer.elapsed_s > target_duration_s:
+            # for achieving target time for one period
+            send_notification("Your Break is DONE, get back to work!")
+            window._tomato()
+            window.after_cancel(after_update_timer_5)
+            window.label_for_ticks()
+            window.label_note_5min.config(fg=YELLOW)
+        else:
+            window.update_time_str(timer.formated_str)
+            window.label_for_ticks()
+            after_update_timer_5 = window.after(1000, update_timer_for_5minute)
+        
+
 
 after_update_timer = None
+after_update_timer_5 = None
 def start_timer():
     timer.start = timer.current()
     update_timer()
-    window.update_time_str(timer.formated_str)
-    window.label_for_ticks()
+    timer.start = timer.current()
+    update_timer_for_5minute()
+    sp.call('cls', shell=True)
+    print('we are here')
+
+
 
 window.make_start_but(start_timer)
 window.make_reset_but(reset_but_clicked)

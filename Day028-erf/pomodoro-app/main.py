4314f023cf78c7777a5dd4d5175d4cbaafb66531
erfaw <146765011+erfaw@users.noreply.github.com>
2025-09-29 11:43:20 +0330

    in 'main.py': 1. code import time delete shd, 2. code haye marbot be 'update_timer()' va 'update_timer_5minute()' delete shd, 3. toye function 'count_down()' be jaye call kardane 'window.label_for_ticks()' ke az aval Label misaze, 'window.label_tick.config()' use kardim va call method ro delete kardim, 4. on 'count_down()' return True haro comment kardim , 5. on 'start_timer()' code haye comment shde PREVIOUS WAY OF MINE MIND ro delete kardim.

--- Day028-erf/pomodoro-app/main.py
+++ Day028-erf/pomodoro-app/main.py
@@ -4,7 +4,6 @@ from CONSTANTS_variable import *
 from interface import *
 from time_manager import TimeManager
 from plyer import notification
-import time
 
 window = PomodoroWindow()
 timer = TimeManager()
@@ -30,66 +29,6 @@ def reset_but_clicked():
     window.label_tick.grid(row=4, column=1)
     window.is_reset_clicked = False
     window.after_cancel(after_id)
-
-# def update_timer():
-#     global after_update_timer
-#     target_duration_s = 25*60 # 25 minute
-#     # target_duration_s = 25*60 # 25 minute
-#     # break_time_s = 10 # 5 minute
-#     # break_time_s = 5*60 # 5 minute
-#     timer.elapsed_s = timer.elapsed(timer.start)
-#     timer.formated_str = timer.format_time(timer.elapsed_s)
-#     window._tomato()
-
-#     if window.is_reset_clicked:
-#         window._raw_time_str()
-#         window.pomodoro_round = 0
-#         window.label_tick.config(fg= YELLOW) #for remove it from display
-#         window.is_reset_clicked = False
-#         window.after_cancel(after_update_timer)
-#     else:
-#         if timer.elapsed_s > target_duration_s:
-#             # for achieving target time for one period
-#             window.pomodoro_round += 1
-#             send_notification("You must take a short break! (5min)")
-#             window._tomato()
-#             window.label_note_5min.config(fg=RED)
-#             window.label_for_ticks()
-#             window.after_cancel(after_update_timer)
-#             # time.sleep(break_time_s)
-#             # send_notification("Your Break is DONE, get back to work")
-#             timer.start= timer.current() 
-#         else:
-#             window.update_time_str(timer.formated_str)
-#             window.label_for_ticks()
-#             after_update_timer = window.after(1000, update_timer)
-
-# def update_timer_for_5minute():
-#     #ّFAILEDTODO: use 'elapsed_reverse' here to count-down timer 
-#     global after_update_timer_5
-#     target_duration_s = 5*60 # must be 5 minute
-#     timer.elapsed_s = timer.elapsed(timer.start)
-#     timer.formated_str = timer.format_time(timer.elapsed_s)
-#     window._tomato()
-
-#     if window.is_reset_clicked:
-#         window._raw_time_str()
-#         window.pomodoro_round = 0
-#         window.label_tick.config(fg= YELLOW) #for remove it from display
-#         window.is_reset_clicked = False
-#         window.after_cancel(after_update_timer_5)
-#     else:
-#         if timer.elapsed_s > target_duration_s:
-#             # for achieving target time for one period
-#             send_notification("Your Break is DONE, get back to work!")
-#             window._tomato()
-#             window.after_cancel(after_update_timer_5)
-#             window.label_for_ticks()
-#             window.label_note_5min.config(fg=YELLOW)
-#         else:
-#             window.update_time_str(timer.formated_str)
-#             window.label_for_ticks()
-#             after_update_timer_5 = window.after(1000, update_timer_for_5minute)
         
 def count_down(count):
     global after_id
@@ -104,25 +43,22 @@ def count_down(count):
         print("You must take a short break! (5min)")
         window.pomodoro_round += 1
         window.is_break_time = True
-        window.label_for_ticks()
+        window.label_tick.config(text=check_mark_char * window.pomodoro_round)
+        window.label_tick.grid(row=4, column=1) 
         window.after_cancel(after_id)
-        return True
+        count_down(20)
+        # return True
     elif count == 0 and window.is_break_time:
         # send_notification("Your Break is DONE, get back to work!")
         print("Your Break is DONE, get back to work!")
         window.is_break_time = False
         window.after_cancel(after_id)
-        return True
+        # return True
 
 
 # after_update_timer = None
 # after_update_timer_5 = None
 def start_timer():
-    # PREVIOUS WAY OF MINE MIND:
-        # timer.start = timer.current()
-        # # update_timer()
-        # timer.start = timer.current()
-        # # update_timer_for_5minute()
     count_down(10) 
 
 

5b5a1726ccab22cf16e9795a1c9bf79bc035ddfe
erfaw <146765011+erfaw@users.noreply.github.com>
2025-09-30 10:59:30 +0330

    in 'time_manager.py': 1. code ha besorate kamel comment shd, 2. methode 'format_time()' copy shd va move shd be 'interface.py' va toye class 'PomodoroWindow()' implement shd.

--- Day028-erf/pomodoro-app/time_manager.py
+++ Day028-erf/pomodoro-app/time_manager.py
@@ -1,24 +1,18 @@
-import time
-class TimeManager:
-    def __init__(self):
-        self.start = None
-        self.elapsed_s = None
-        self.formated_str = None
+# import time
+# class TimeManager:
+#     def __init__(self):
+#         self.start = None
+#         self.elapsed_s = None
+#         self.formated_str = None
 
-    def current(self):
-        """return current time"""
-        return time.time() #current moment
+#     def current(self):
+#         """return current time"""
+#         return time.time() #current moment
     
-    def format_time(self, time_obj):
-        """return %M:%S formated string of time recieved as arg"""
-        return time.strftime("%M:%S", time.gmtime(time_obj))
+#     def format_time(self, time_obj):
+#         """return %M:%S formated string of time recieved as arg"""
+#         return time.strftime("%M:%S", time.gmtime(time_obj))
 
-    def elapsed(self, start_t):
-        """recieve an time object and calculate elapsed time based on time object and return integer elapsed time value -->int"""
-        return int(time.time() - start_t)
-
-    #FAILEDTODO: make elapsed_reverse functional to function right for count-down
-    # def elapsed_reverse(self, target):
-    #     return int(
-    #         target - time.time()
-    #     )
+#     def elapsed(self, start_t):
+#         """recieve an time object and calculate elapsed time based on time object and return integer elapsed time value -->int"""
+#         return int(time.time() - start_t)

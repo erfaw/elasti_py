cef23aa3c8ebcee945bf468f1ac5d111dbe9c6b5
erfaw <146765011+erfaw@users.noreply.github.com>
2025-10-01 10:55:04 +0330

    window sakhte shd, va maghadir tebgh course set shd, alan ye aks darim vasate ye tkwindow

--- Day029-erf/Password-Manager-app/main.py
+++ Day029-erf/Password-Manager-app/main.py
@@ -1,23 +1,28 @@
+from tkinter import *
+import subprocess as sp; sp.call('cls', shell= True)
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
-from tkinter import *
-import subprocess as sp; sp.call('cls', shell= True)
-root = Tk()
+window = Tk()
+window.title('Password Manager')
+window.config(
+    padx= 20,
+    pady= 20,
+)
 
 canvas = Canvas(
     width = 200,
-    height = 200, 
+    height = 200,
 )
 bg_img = PhotoImage(file='./Day029-erf/Password-Manager-app/logo.png')
 
 canvas.create_image(
-     100,
-     100,
+     100, # middle of bg_img x
+     100, # middle of bg_img y
     image= bg_img,
 )
 canvas.pack()
 
-root.mainloop()
+window.mainloop()

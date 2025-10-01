9e4ba1346b5ff2be7c420cd6aeb5ea5ed7bbf938
erfaw <146765011+erfaw@users.noreply.github.com>
2025-10-01 11:38:05 +0330

    in 'main.py': 1. tamami UI build shd ba maghadire drst, (cherake maghadir default dade shode tavasote course roye mac javab midad roye windows drst nabod), 2. center image ham roye x dar nazar gereftim 80 ke biad vasat tar vayse.

--- Day029-erf/Password-Manager-app/main.py
+++ Day029-erf/Password-Manager-app/main.py
@@ -8,8 +8,8 @@ import subprocess as sp; sp.call('cls', shell= True)
 window = Tk()
 window.title('Password Manager')
 window.config(
-    padx= 20,
-    pady= 20,
+    padx= 55,
+    pady= 55,
 )
 
 canvas = Canvas(
@@ -19,10 +19,37 @@ canvas = Canvas(
 bg_img = PhotoImage(file='./Day029-erf/Password-Manager-app/logo.png')
 
 canvas.create_image(
-     100, # middle of bg_img x
+     80, # middle of bg_img x
      100, # middle of bg_img y
     image= bg_img,
 )
-canvas.pack()
+canvas.grid(row=0, column=1)
+
+website_text = Label(
+    text= 'Website: '
+    ,justify= 'right',
+)
+website_text.grid(row=1, column=0)
+
+website_entry = Entry(width= 42, justify='left')
+website_entry.grid(row=1, column=1, columnspan= 2)
+
+email_username_text = Label(
+    text= 'Email/Username: ',justify= 'right',
+)
+email_username_text.grid(row=2, column= 0)
+
+email_username_entry = Entry(width= 42, justify='left', )
+email_username_entry.grid(row=2, column= 1, columnspan= 2)
+
+password_text = Label(text='Password :',justify= 'right',)
+password_text.grid(row=3, column=0)
+password_entry = Entry(width=21+11, justify='left')
+password_entry.grid(row=3, column= 1, padx=0)
+generate_btn = Button(text='Generate', justify='left', highlightthickness= 0, )
+generate_btn.grid(row=3, column=2, padx=0)
+
+add_btn = Button(text='Add', width= 35, justify='left')
+add_btn.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()

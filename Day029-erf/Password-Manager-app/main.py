f6c11cd1bf1332bfb07cb5109858dbb7a4b0f775
erfaw <146765011+erfaw@users.noreply.github.com>
2025-10-01 11:58:53 +0330

    in 'main.py': 1. add black background color, 2. add background color to entry for password, 3. add background color for buttons at all some changes on colors

--- Day029-erf/Password-Manager-app/main.py
+++ Day029-erf/Password-Manager-app/main.py
@@ -10,11 +10,15 @@ window.title('Password Manager')
 window.config(
     padx= 55,
     pady= 55,
+    bg= "#454545"
 )
+window.resizable(False, False)
 
 canvas = Canvas(
     width = 200,
     height = 200,
+    bg= "#454545",
+    highlightthickness=0
 )
 bg_img = PhotoImage(file='./Day029-erf/Password-Manager-app/logo.png')
 
@@ -26,30 +30,40 @@ canvas.create_image(
 canvas.grid(row=0, column=1)
 
 website_text = Label(
-    text= 'Website: '
-    ,justify= 'right',
+    text= 'Website: ',
+    justify= 'right',
+    bg= "#454545",
+    fg= 'white'
 )
 website_text.grid(row=1, column=0)
 
 website_entry = Entry(width= 42, justify='left')
-website_entry.grid(row=1, column=1, columnspan= 2)
+website_entry.grid(row=1, column=1, columnspan= 2, pady=2)
 
 email_username_text = Label(
-    text= 'Email/Username: ',justify= 'right',
+    text= 'Email/Username: ',
+    justify= 'right',
+    bg= "#454545",
+    fg= 'white'    
 )
 email_username_text.grid(row=2, column= 0)
 
-email_username_entry = Entry(width= 42, justify='left', )
-email_username_entry.grid(row=2, column= 1, columnspan= 2)
+email_username_entry = Entry(width= 42, justify='left' )
+email_username_entry.grid(row=2, column= 1, columnspan= 2, pady=2)
 
-password_text = Label(text='Password :',justify= 'right',)
+password_text = Label(
+    text='Password :',
+    justify= 'right',
+    bg= "#454545",
+    fg= 'white'    
+    )
 password_text.grid(row=3, column=0)
-password_entry = Entry(width=21+11, justify='left')
-password_entry.grid(row=3, column= 1, padx=0)
-generate_btn = Button(text='Generate', justify='left', highlightthickness= 0, )
-generate_btn.grid(row=3, column=2, padx=0)
+password_entry = Entry(width=21+11, justify='left', bg="#D4483B")
+password_entry.grid(row=3, column= 1, padx=0, pady=2)
+generate_btn = Button(text='Generate', justify='left', highlightthickness= 0, bg= "#5c85d6" )
+generate_btn.grid(row=3, column=2, padx=0, pady=2)
 
-add_btn = Button(text='Add', width= 35, justify='left')
-add_btn.grid(row=4, column=1, columnspan=2)
+add_btn = Button(text='Add', width= 35, justify='left', bg= "#5c85d6")
+add_btn.grid(row=4, column=1, columnspan=2, pady=2)
 
 window.mainloop()

cd514e688c2c4d286bae331b5f0b540a1364248d
erfaw <146765011+erfaw@users.noreply.github.com>
2025-10-03 12:16:44 +0330

    remove comments and extra codes

--- Day030-erf/NATO-alphabet-start/main.py
+++ Day030-erf/NATO-alphabet-start/main.py
@@ -6,24 +6,7 @@ student_dict = {
     "score": [56, 76, 98]
 }
 
-#Looping through dictionaries:
-for (key, value) in student_dict.items():
-    #Access key and value
-    pass
-
-# student_data_frame = pd.DataFrame(student_dict)
-
-#Loop through rows of a data frame
-# for (index, row) in student_data_frame.iterrows():
-#     #Access index and row
-#     #Access row.student or row.score
-#     pass
-
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
-
 #DONE 1. Create a dictionary in this format:
-# {"A": "Alfa", "B": "Bravo"}
 nato_db = pd.read_csv("./Day026-erf/NATO-alphabet-start/nato_phonetic_alphabet.csv")
 formated_dictionary_nato = {row.letter: row.code for (index, row) in nato_db.iterrows()}
 

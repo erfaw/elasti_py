8cdff6f5b496f9e6790235c8942f2ce1d8e8f491
erfaw <146765011+erfaw@users.noreply.github.com>
2025-10-04 10:26:27 +0330

    change name of folder 'NATO-alphabet-start' to 'NATO-alphabet-start-improved'

---
+++ Day030-erf/NATO-alphabet-start-improved/main.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import subprocess; subprocess.call('cls', shell=True)
+
+#DONE 1. Create a dictionary in this format:
+nato_db = pd.read_csv("./Day026-erf/NATO-alphabet-start/nato_phonetic_alphabet.csv")
+formated_dictionary_nato = {row.letter: row.code for (index, row) in nato_db.iterrows()}
+
+#DONE 2. Create a list of the phonetic code words from a word that the user inputs.
+def check_input():
+    global user_word 
+    u_w = input('Please enter your name or message:\n')
+    try:
+        for letter in u_w:
+            formated_dictionary_nato[letter.upper()]
+    except KeyError:
+        print("Sorry, only letters will be expected!")
+        check_input()
+    else:
+        user_word = u_w
+    
+# user_word = input('Please enter your name or message:\n') 
+check_input() 
+user_word_splited_list = list(user_word)
+
+print(user_word_splited_list)
+nato_for_user = [formated_dictionary_nato[f_char.upper()] for f_char in user_word_splited_list]
+print(nato_for_user)

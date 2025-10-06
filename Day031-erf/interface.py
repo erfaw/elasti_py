bf25799447d998774857b05eca4d8b77fc050780
erfaw <146765011+erfaw@users.noreply.github.com>
2025-10-06 11:52:50 +0330

    in 'interface.py': 1. file baraye develope UI sakhtim, 2. class 'FlashyWindow()' ro sakhtim, 3. canvas sakhtim baraye card (front/back), 4. button right and wrong ba image hayi ke dashtan sakhtim,

---
+++ Day031-erf/interface.py
@@ -0,0 +1,46 @@
+from tkinter import *
+class FlashyWindow(Tk):
+    def __init__(self):
+        super().__init__()
+        self.BACKGROUND_COLOR = "#B1DDC6"
+        self.FONTI = ("Ariel", 40, "italic")
+        self.card_img = PhotoImage(file="./Day031-erf/images/card_front.png")
+        self.right_img = PhotoImage(file="./Day031-erf/images/right.png")
+        self.wrong_img = PhotoImage(file="./Day031-erf/images/wrong.png")
+        self.make_root_window()
+        self.make_card_canvas()
+        self.make_right_btn()
+        self.make_wrong_btn()
+
+    def make_root_window(self):
+        self.title("Flashy")
+        self.config(
+            width=800,
+            height=526,
+            padx=50,
+            pady=50,
+            bg= self.BACKGROUND_COLOR
+            )
+        
+    def make_card_canvas(self):
+        self.canvas = Canvas(width=800, height=526, highlightthickness=0, bg=self.BACKGROUND_COLOR)
+        self.canvas.create_image(400, 262, image= self.card_img)
+        self.canvas.grid(row=0, column=0, columnspan=2)
+
+    def make_right_btn(self):
+        self.right_btn = Button()
+        self.right_btn.config(
+            image= self.right_img,
+            highlightthickness=0
+        )
+        self.right_btn.grid(row=1, column=1)
+
+    def make_wrong_btn(self):
+        self.wrong_btn = Button()
+        self.wrong_btn.config(
+            image= self.wrong_img,
+            highlightthickness=0
+        )
+        self.wrong_btn.grid(row=1, column=0)        
+
+    

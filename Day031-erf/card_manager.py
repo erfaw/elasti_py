67b26afbacf3aaddf6918936e8323840c653019b
erfaw <146765011+erfaw@users.noreply.github.com>
2025-10-07 12:46:21 +0330

    on 'card_manager.py': 1. on 'load_csv_file()': try-except add kardim baraye inke words_to_learn.csv ro bekhonim va agar nist az roye 1k_En_Per.csv besazimesh 2. methode 'delete_record()' sakhte shd, (empty)

--- Day031-erf/card_manager.py
+++ Day031-erf/card_manager.py
@@ -6,7 +6,18 @@ class CardManager():
         self.show_card = None
 
     def load_csv_file(self):
-        return pd.read_csv("./Day031-erf/data/1k_En_Per.csv").to_dict(orient='records')
+        try:
+            f=open('./Day031-erf/data/words_to_learn.csv', mode='r')
+        except FileNotFoundError:
+            raw_data = pd.read_csv("./Day031-erf/data/1k_En_Per.csv")
+            raw_data.to_csv('./Day031-erf/data/words_to_learn.csv')
+        else:
+            f.close()
+        finally:
+            return pd.read_csv("./Day031-erf/data/words_to_learn.csv").to_dict(orient='records')
          
     def random_choice(self):
         return choice(self.data)
+    
+    def delete_record(self):
+        pass

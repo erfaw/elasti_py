0b5ced57fb6a7beda64541b826f3ed49044f3b1f
erfaw <146765011+erfaw@users.noreply.github.com>
2025-10-07 11:59:38 +0330

    in 'main.py': 1. on 'push_right_btn()': ezafe kardim harbar ke btn click mikhore, bg_img config beshe, 2. on 'push_right_btn()': add karidm fa .after() baad az 3s func 'show_flip()' ro call kne, 3. function 'show_flip()' ro sakhtim, 4. on 'show_flip()': bg card ro be 'window.card_back_img' taghir ddim, 'window.title_str' ro 'Persian' kardim, 'window.word_str' ro ba kalame farsi change kardim,

--- Day031-erf/main.py
+++ Day031-erf/main.py
@@ -5,8 +5,29 @@ import subprocess as sp; sp.call('cls', shell= True)
 window = FlashyWindow()
 cards = CardManager()
 
+def show_flip():
+    # change bg color done
+    window.canvas.itemconfig(
+        window.card_bg,
+        image= window.card_back_img
+    )
+    # change title_str done
+    window.canvas.itemconfig(
+        window.title_str,
+        text= 'Persian'
+    )
+    # change word_str to persian 
+    window.canvas.itemconfig(
+        window.word_str,
+        text= cards.show_card['Persian']
+    )
+
 def push_right_btn():
     cards.show_card = cards.random_choice()
+    window.canvas.itemconfig(
+        window.card_bg,
+        image= window.card_front_img
+    )
     window.canvas.itemconfig(
         window.title_str,
         text = 'English'
@@ -15,6 +36,7 @@ def push_right_btn():
         window.word_str,
         text = cards.show_card['English'].title()
     )
+    window.after(3000, show_flip)
 
 window.right_btn.config(command=push_right_btn)
 window.wrong_btn.config(command=push_right_btn)

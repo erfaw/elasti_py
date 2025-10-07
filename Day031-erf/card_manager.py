2dbc0e0def16b8d63b3c27a783ba2ead974d91ed
erfaw <146765011+erfaw@users.noreply.github.com>
2025-10-07 11:21:39 +0330

    in 'card_manager.py': 1. on 'load_csv_file()' code ro yek ja return kardim, 2. methode 'random_choice()' sakhte shd, ba estefade az random.choice ye element return mikne az data card ha,

--- Day031-erf/card_manager.py
+++ Day031-erf/card_manager.py
@@ -1,8 +1,11 @@
 import pandas as pd
+from random import choice
 class CardManager():
     def __init__(self):
         self.data = self.load_csv_file()
 
     def load_csv_file(self):
-        db = pd.read_csv("./Day031-erf/data/1k_En_Per.csv").to_dict(orient='records')
-        return db
+        return pd.read_csv("./Day031-erf/data/1k_En_Per.csv").to_dict(orient='records')
+         
+    def random_choice(self):
+        return choice(self.data)

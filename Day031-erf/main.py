654de95d8f16d01e02da345b27b54d33fadad26b
erfaw <146765011+erfaw@users.noreply.github.com>
2025-10-07 11:37:19 +0330

    in 'main.py': 1. subprocess baraye cls add shd, nothing, 2. function 'push_right_btn()' add shd, 3. on 'push_right_btn()' ye random card choice mishe, tebghe on 'window.word_str' change mishe, 4. code haye 'window.right_btn' and 'widnow.wrong_btn' config shd be command=push_right_btn (baraye 004 challenge, gharare har btn ro mizani ye kalame new neshon bede) badan taghir mikne,

--- Day031-erf/main.py
+++ Day031-erf/main.py
@@ -1,9 +1,23 @@
 from interface import FlashyWindow
 from card_manager import CardManager
+import subprocess as sp; sp.call('cls', shell= True)
 #DONE: make UI
 window = FlashyWindow()
 cards = CardManager()
 
+def push_right_btn():
+    cards.show_card = cards.random_choice()
+    window.canvas.itemconfig(
+        window.title_str,
+        text = 'English'
+    )
+    window.canvas.itemconfig(
+        window.word_str,
+        text = cards.show_card['English'].title()
+    )
+
+window.right_btn.config(command=push_right_btn)
+window.wrong_btn.config(command=push_right_btn)
 
 
 window.mainloop()

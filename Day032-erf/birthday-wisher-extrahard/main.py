b73933a24c4f9f2f596c29d827bfe20e2fd592d0
erfaw <146765011+erfaw@users.noreply.github.com>
2025-10-10 21:03:53 +0330

    in 'main.py': 1. done the todo : Check if today matches a birthday in the birthdays.csv

--- Day032-erf/birthday-wisher-extrahard/main.py
+++ Day032-erf/birthday-wisher-extrahard/main.py
@@ -1,8 +1,18 @@
 ##################### Extra Hard Starting Project ######################
-
+import subprocess as sp; sp.call('cls', shell=True)
+import pandas as pd
+import datetime as dt
 #DONE: Update the birthdays.csv
 
-#TODO: Check if today matches a birthday in the birthdays.csv
+#DONE: Check if today matches a birthday in the birthdays.csv
+data = pd.read_csv("./Day032-erf/birthday-wisher-extrahard/birthdays.csv")
+# print(data)
+now = dt.datetime.now()
+
+birthday_matched = data[(data.month == now.month) & (data.day == now.day)]
+
+print(birthday_matched)
+
 
 #TODO: If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
 

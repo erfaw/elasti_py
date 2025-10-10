f63e15af11d027cdf6299a42437a53aa13f04ecf
erfaw <146765011+erfaw@users.noreply.github.com>
2025-10-10 21:33:55 +0330

    in 'main.py': 1. done the TODO: 'If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv'

--- Day032-erf/birthday-wisher-extrahard/main.py
+++ Day032-erf/birthday-wisher-extrahard/main.py
@@ -2,19 +2,25 @@
 import subprocess as sp; sp.call('cls', shell=True)
 import pandas as pd
 import datetime as dt
+from random import randint
 #DONE: Update the birthdays.csv
 
 #DONE: Check if today matches a birthday in the birthdays.csv
 data = pd.read_csv("./Day032-erf/birthday-wisher-extrahard/birthdays.csv")
-# print(data)
 now = dt.datetime.now()
-
 birthday_matched = data[(data.month == now.month) & (data.day == now.day)]
 
-print(birthday_matched)
+#DONE: If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
 
+# pick a random letter
+with open(f'./Day032-erf/birthday-wisher-extrahard/letter_templates/letter_{randint(1,3)}.txt') as file:
+    letter_str = file.read()
+print(birthday_matched['name'].values)
 
-#TODO: If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+#replace [NAME]
+letter_str = letter_str.replace('[NAME]', birthday_matched['name'].to_string(index=False).capitalize())
+print(letter_str)
+
 
 #TODO: Send the letter generated in step 3 to that person's email address.
 

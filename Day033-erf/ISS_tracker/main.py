70ecc70b6c853aca43ed06da2908d799d5fb94a2
erfaw <146765011+erfaw@users.noreply.github.com>
2025-10-12 14:04:09 +0330

    in 'main.py': 1. add TODOs

--- Day033-erf/ISS_tracker/main.py
+++ Day033-erf/ISS_tracker/main.py
@@ -39,3 +39,8 @@ print(f"ISS now coordinate is (lat,lng) ===> {iss_latitude} , {iss_longitude}")
 #DONE: parse data to 24h formated
 #DONE: catch now.hour 
 #DONE: added ISS_coordinate codes here, and print it
+
+#TODO: catch the moment which ISS is close to my current location (-5 +5)
+#TODO: check if is it currently dark or not (it must be after sunset and before sunrise)
+#TODO: if was: send an email to tell me look up
+#TODO: RUN the code every 60s

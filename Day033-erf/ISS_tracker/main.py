0d7e21467e01a543bab078ee05c3aaa960ea3761
erfaw <146765011+erfaw@users.noreply.github.com>
2025-10-12 13:42:25 +0330

    in 'main.py': 1. DONE TODO: added ISS_coordinate codes here, and print it,

--- Day033-erf/ISS_tracker/main.py
+++ Day033-erf/ISS_tracker/main.py
@@ -1,6 +1,16 @@
 import subprocess as sp; sp.call('cls', shell=True)
 import requests
 from datetime import datetime
+# catch data from ISS coordinate from api
+res_iss = requests.get("http://api.open-notify.org/iss-now.json")
+res_iss.raise_for_status()
+data = res_iss.json()
+
+ISS_coordinate = (
+    data['iss_position']['latitude'],
+    data['iss_position']['longitude']
+)
+
 URL = 'https://api.sunrise-sunset.org/json'
 ARAK_LATITUDE = 34.092229
 ARAK_LONGITUDE = 49.721958
@@ -23,9 +33,11 @@ print(f"sunset hour is ===> {sunset_hour}")
 
 current_hour = datetime.now().hour
 print(f"current hour is ===> {current_hour}")
+print(f"ISS now coordinate is (lat,lng) ===> {ISS_coordinate}")
 
 
 # ---- TODOS ---- #
 #DONE: catch the data from api related to sunrise and sunset of arak
 #DONE: parse data to 24h formated
 #DONE: catch now.hour 
+#DONE: added ISS_coordinate codes here, and print it

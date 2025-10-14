8eb19f7dce5478732f61fa4ecda0fb55f4d15da9
erfaw <146765011+erfaw@users.noreply.github.com>
2025-10-14 11:39:01 +0330

    in 'main.py': 1. funciton 'is_dark_sky()' declared, 2. add a print and call 'is_dark_sky()', 3. DONE TODO: check if is it currently dark or not (it must be after sunset and before sunrise),

--- Day033-erf/ISS_tracker/main.py
+++ Day033-erf/ISS_tracker/main.py
@@ -15,20 +15,26 @@ sun.fill_hour_atts()
 
 current_hour = datetime.now().hour
 
+def is_dark_sky():
+    """return True if sky is dark and we could see a space station"""
+    if sun.sunset_hour < current_hour < sun.sunrise_hour :
+        return True
+    else: 
+        return False
+    
 print(f"sunrise hour is ===> {sun.sunrise_hour}")
 print(f"sunset hour is ===> {sun.sunset_hour}")
 print(f"current hour is ===> {current_hour}")
 print(f"ISS now coordinate is (lat,lng) ===> {iss.latitude} , {iss.longitude}")
 print(f"is ISS close to me? ==> {iss.is_it_close(lat= ARAK_LATITUDE, lng= ARAK_LONGITUDE)}")
-
+print(f"is it dark enough to see space station? ==> {is_dark_sky()}")
 # ---- TODOS ---- #
 #DONE: catch the data from api related to sunrise and sunset of arak
 #DONE: parse data to 24h formated
 #DONE: catch now.hour 
 #DONE: added ISS_coordinate codes here, and print it
 #DONE: OOP all of ISS and Sun
-
 #DONE: catch the moment which ISS is close to my current location (-5 +5)
-#TODO: check if is it currently dark or not (it must be after sunset and before sunrise)
+#DONE: check if is it currently dark or not (it must be after sunset and before sunrise)
 #TODO: if was: send an email to tell me look up
 #TODO: RUN the code every 60s

5127e90492a9c7b272376fa967a184749faaca66
erfaw <146765011+erfaw@users.noreply.github.com>
2025-10-14 12:22:02 +0330

    in 'iss_track.py': 1. in 'get_cor()' add try-except statement to catch exception: requests.exceptions.ConnectTimeout

--- Day033-erf/ISS_tracker/iss_track.py
+++ Day033-erf/ISS_tracker/iss_track.py
@@ -6,8 +6,11 @@ class ISS_Track:
 
     def get_cor(self):
         "get now coordinate of ISS from api, then fill it to self.latitude and self.longitude"
-        res_iss = requests.get("http://api.open-notify.org/iss-now.json")
-        res_iss.raise_for_status()
+        try:
+            res_iss = requests.get("http://api.open-notify.org/iss-now.json")
+            res_iss.raise_for_status()
+        except requests.exceptions.ConnectTimeout as er_ms:
+            print(f'Error: request timed out!\nerror message:\n{er_ms}')
         data = res_iss.json()
         self.latitude = float(data["iss_position"]["latitude"])
         self.longitude = float(data["iss_position"]["longitude"])   

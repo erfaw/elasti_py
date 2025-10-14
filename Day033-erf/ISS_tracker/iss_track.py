ec9de32cd904a1843a6c243c09ae13938a51ab8a
erfaw <146765011+erfaw@users.noreply.github.com>
2025-10-14 10:41:14 +0330

    on 'iss_track.py': 1. on 'get_cor()' lat and lng filled in 'self.latitude' and 'self.longitude' attributes, 2. on 'get_cor()' docstring updated for this functionality

--- Day033-erf/ISS_tracker/iss_track.py
+++ Day033-erf/ISS_tracker/iss_track.py
@@ -5,10 +5,10 @@ class ISS_Track:
         self.longitude = None
 
     def get_cor(self):
-        "get now coordinate of ISS from api, then return it as a tuple -> (lat,lng)"
+        "get now coordinate of ISS from api, then fill it to self.latitude and self.longitude"
         res_iss = requests.get("http://api.open-notify.org/iss-now.json")
         res_iss.raise_for_status()
         data = res_iss.json()
-        iss_latitude = float(data["iss_position"]["latitude"])
-        iss_longitude = float(data["iss_position"]["longitude"])   
-        return (iss_latitude, iss_longitude)
+        self.latitude = float(data["iss_position"]["latitude"])
+        self.longitude = float(data["iss_position"]["longitude"])   
+        

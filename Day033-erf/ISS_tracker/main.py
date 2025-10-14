4b0eba7e02e3871c87b37c6047f24a4979eda764
erfaw <146765011+erfaw@users.noreply.github.com>
2025-10-14 11:16:15 +0330

    in 'main.py': 1. add 'ARAK_LATITUDE' and 'ARAK_LONGITUDE' here (was in 'sun.py'), 2. sended as kwrgs to 'sun.get_situation'

--- Day033-erf/ISS_tracker/main.py
+++ Day033-erf/ISS_tracker/main.py
@@ -3,12 +3,14 @@ import requests
 from datetime import datetime
 from iss_track import ISS_Track
 from sun import SunSituation
+ARAK_LATITUDE = 34.092229
+ARAK_LONGITUDE = 49.721958
 
 iss = ISS_Track()
 iss.get_cor()
 
 sun = SunSituation()
-sun.get_situation()
+sun.get_situation(lat= ARAK_LATITUDE, lng= ARAK_LONGITUDE)
 sun.fill_hour_atts()
 
 current_hour = datetime.now().hour
@@ -19,11 +21,13 @@ print(f"current hour is ===> {current_hour}")
 print(f"ISS now coordinate is (lat,lng) ===> {iss.latitude} , {iss.longitude}")
 
 
+
 # ---- TODOS ---- #
 #DONE: catch the data from api related to sunrise and sunset of arak
 #DONE: parse data to 24h formated
 #DONE: catch now.hour 
 #DONE: added ISS_coordinate codes here, and print it
+#DONE: OOP all of ISS and Sun
 
 #TODO: catch the moment which ISS is close to my current location (-5 +5)
 #TODO: check if is it currently dark or not (it must be after sunset and before sunrise)

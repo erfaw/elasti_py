c0086192c2d68511239979c70241fbecaa7051ea
erfaw <146765011+erfaw@users.noreply.github.com>
2025-10-14 10:58:35 +0330

    in 'sun.py': 1. made 'data', 'sunrise_hour', 'sunset_hour' attributes, filled None 2. make 'get_situation()' method to req api and fill 'data', 3. make 'fill_hour_atts()' method to tap into 'data' and catch Hour integer,

--- Day033-erf/ISS_tracker/sun.py
+++ Day033-erf/ISS_tracker/sun.py
@@ -1,3 +1,28 @@
+import requests
 class SunSituation:
     def __init__(self):
-        pass
+        self.data = None
+        self.sunrise_hour = None
+        self.sunset_hour = None
+
+    def get_situation(self):
+        """request to api and get and fill self.data"""
+        URL = 'https://api.sunrise-sunset.org/json'
+        ARAK_LATITUDE = 34.092229
+        ARAK_LONGITUDE = 49.721958
+        TIME_ZONE = 'Asia/Tehran'
+        PARAMETERS = { # For arak
+            'lat': ARAK_LATITUDE,
+            'lng': ARAK_LONGITUDE,
+            'tzid': TIME_ZONE,
+            'formatted': 0
+        }  
+        response = requests.get(url=URL, params=PARAMETERS)
+        response.raise_for_status()
+        self.data = response.json()
+        
+
+    def fill_hour_atts(self):
+        """tap into self.data and catch Hour integer for sunrise and sunset and fill it to 'self.sunrise_hour' and 'self.sunset_hour'"""
+        self.sunrise_hour = int(self.data['results']['sunrise'].split('T')[1].split(':')[0])
+        self.sunset_hour = int(self.data['results']['sunset'].split('T')[1].split(':')[0])  

a0066398ae8bcfe47fe66da232327083333510d5
erfaw <146765011+erfaw@users.noreply.github.com>
2025-10-14 12:23:40 +0330

    in 'main.py': 1. reorder the code to add 'main()' function for callback, 2. import threading, 3. move parts of code which need repeat to 'main()', 4. call 'main()' at the end of code,

--- Day033-erf/ISS_tracker/main.py
+++ Day033-erf/ISS_tracker/main.py
@@ -1,5 +1,6 @@
 import subprocess as sp; sp.call('cls', shell=True)
 import requests
+import threading
 from datetime import datetime
 from iss_track import ISS_Track
 from sun import SunSituation
@@ -7,15 +8,8 @@ from mail_sender import GmailSender
 ARAK_LATITUDE = 34.092229
 ARAK_LONGITUDE = 49.721958
 mail = GmailSender()
-
 iss = ISS_Track()
-iss.get_cor()
-
 sun = SunSituation()
-sun.get_situation(lat= ARAK_LATITUDE, lng= ARAK_LONGITUDE)
-sun.fill_hour_atts()
-
-current_hour = datetime.now().hour
 
 def is_dark_sky():
     """return True if sky is dark and we could see a space station"""
@@ -23,31 +17,42 @@ def is_dark_sky():
         return True
     else: 
         return False
-    
-print(f"sunrise hour is ===> {sun.sunrise_hour}")
-print(f"sunset hour is ===> {sun.sunset_hour}")
-print(f"current hour is ===> {current_hour}")
-print(f"ISS now coordinate is (lat,lng) ===> {iss.latitude} , {iss.longitude}")
-print(f"is ISS close to me? ==> {iss.is_it_close(lat= ARAK_LATITUDE, lng= ARAK_LONGITUDE)}")
-print(f"is it dark enough to see space station? ==> {is_dark_sky()}")
 
-if iss.is_it_close(lat= ARAK_LATITUDE, lng= ARAK_LONGITUDE) and is_dark_sky():
-    mail.send(
-        sender_email= "erfawn.h@gmail.com",
-        sender_app_password= "givl ankx ezxx dxih",
-        recipient_email= "erfawn.h@gmail.com",
-        subject= "LOOK UPPPP!!!!",
-        message_to_send= "ISS is just right up of your head, go and see it :) !"
-    )
-else: pass
-    # mail.send(
-    # sender_email= "erfawn.h@gmail.com",
-    # sender_app_password= "givl ankx ezxx dxih",
-    # recipient_email= "erfawn.h@gmail.com",
-    # subject= "DONE LOOK!",
-    # message_to_send= "after many circumstances you cant see it right now!"
-    # )
+def main():
+    global current_hour
+    sp.call('cls', shell=True)
+    iss.get_cor()
+    sun.get_situation(lat= ARAK_LATITUDE, lng= ARAK_LONGITUDE)
+    sun.fill_hour_atts()
+    current_hour = datetime.now().hour
 
+    print(f"sunrise hour is ===> {sun.sunrise_hour}")
+    print(f"sunset hour is ===> {sun.sunset_hour}")
+    print(f"current hour is ===> {current_hour}")
+    print(f"ISS now coordinate is (lat,lng) ===> {iss.latitude} , {iss.longitude}")
+    print(f"is ISS close to me? ==> {iss.is_it_close(lat= ARAK_LATITUDE, lng= ARAK_LONGITUDE)}")
+    print(f"is it dark enough to see space station? ==> {is_dark_sky()}")
+
+    if iss.is_it_close(lat= ARAK_LATITUDE, lng= ARAK_LONGITUDE) and is_dark_sky():
+        mail.send(
+            sender_email= "erfawn.h@gmail.com",
+            sender_app_password= "givl ankx ezxx dxih",
+            recipient_email= "erfawn.h@gmail.com",
+            subject= "LOOK UPPPP!!!!",
+            message_to_send= "ISS is just right up of your head, go and see it :) !"
+        )
+    else: 
+        print('NOT HERE')
+        # mail.send(
+        # sender_email= "erfawn.h@gmail.com",
+        # sender_app_password= "givl ankx ezxx dxih",
+        # recipient_email= "erfawn.h@gmail.com",
+        # subject= "DONE LOOK!",
+        # message_to_send= "after many circumstances you cant see it right now!"
+        # )
+    threading.Timer(60, main).start()
+
+main()
 
 # ---- TODOS ---- #
 #DONE: catch the data from api related to sunrise and sunset of arak
@@ -58,4 +63,4 @@ else: pass
 #DONE: catch the moment which ISS is close to my current location (-5 +5)
 #DONE: check if is it currently dark or not (it must be after sunset and before sunrise)
 #DONE: if was: send an email to tell me look up
-#TODO: RUN the code every 60s
+#TODO: RUN the code every

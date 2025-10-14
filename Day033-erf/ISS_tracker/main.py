834a6719365bd382318b9d8aa3b6ce9bcab7f1ff
erfaw <146765011+erfaw@users.noreply.github.com>
2025-10-14 11:00:59 +0330

    in 'main.py': 1. from sun.py imported SunSituation() class 2. object 'sun' has been maded, 3. called 'sun.get_situation()' and 'sun.fill_hour_atts()', 4. replace code with this, 5. removed commented codes belong to req api for sunset and sunrise, sun OOP looks good enough

--- Day033-erf/ISS_tracker/main.py
+++ Day033-erf/ISS_tracker/main.py
@@ -2,30 +2,17 @@ import subprocess as sp; sp.call('cls', shell=True)
 import requests
 from datetime import datetime
 from iss_track import ISS_Track
+from sun import SunSituation
 
 iss = ISS_Track()
 iss.get_cor()
 
+sun = SunSituation()
+sun.get_situation()
+sun.fill_hour_atts()
 
-URL = 'https://api.sunrise-sunset.org/json'
-ARAK_LATITUDE = 34.092229
-ARAK_LONGITUDE = 49.721958
-TIME_ZONE = 'Asia/Tehran'
-PARAMETERS = { # For arak
-    'lat': ARAK_LATITUDE,
-    'lng': ARAK_LONGITUDE,
-    'tzid': TIME_ZONE,
-    'formatted': 0
-}
-
-response = requests.get(url=URL, params=PARAMETERS)
-response.raise_for_status()
-data = response.json()
-# parse to catch 24h formated hour
-sunrise_hour = int(data['results']['sunrise'].split('T')[1].split(':')[0])
-sunset_hour = int(data['results']['sunset'].split('T')[1].split(':')[0])
-print(f"sunrise hour is ===> {sunrise_hour}")
-print(f"sunset hour is ===> {sunset_hour}")
+print(f"sunrise hour is ===> {sun.sunrise_hour}")
+print(f"sunset hour is ===> {sun.sunset_hour}")
 
 current_hour = datetime.now().hour
 print(f"current hour is ===> {current_hour}")

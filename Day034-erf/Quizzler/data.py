3885fa2fd5a560c1efc0940728a27f7efb8198df
erfaw <146765011+erfaw@users.noreply.github.com>
2025-10-16 13:56:48 +0330

    in 'data.py': 1. add 2 TODOs

--- Day034-erf/Quizzler/data.py
+++ Day034-erf/Quizzler/data.py
@@ -2,9 +2,11 @@ import requests
 import subprocess as sp; sp.call('cls', shell=True)
 # build a request to api and implement to 'question_data' and then; comment out below
 API = 'https://opentdb.com/api.php'
+#TODO: add some sort of functionality to ui and data to ask the user with GUI which category he like to play
 PARAMETERS = {
     'type': 'boolean',
     'amount': 10
+    #TODO: add a field here name 'category' and fill it with user choice of category he/she like to play with
 }
 res = requests.get(API, PARAMETERS)
 res.raise_for_status()

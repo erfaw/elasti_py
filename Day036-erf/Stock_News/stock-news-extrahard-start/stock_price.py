01857ace4e2ba136d4be10a378691b619e131d92
erfaw <146765011+erfaw@users.noreply.github.com>
2025-10-19 17:48:59 +0330

    in 'stock_price.py': 1. class 'StockPrice()' declared, 2. needed attributes and methods added from 'main.py', 3. all tested done,

---
+++ Day036-erf/Stock_News/stock-news-extrahard-start/stock_price.py
@@ -0,0 +1,36 @@
+import requests, json
+class StockPrice:
+    def __init__(self, stock_name: str, company_name: str, api_key):
+        """geting daily stock price data from 'alphavantage' site"""
+        self.URL_ALPHAVANTAGE_API = "https://www.alphavantage.co/query"
+        self.URL_ALPHAVANTAGE_API_PARAMS = {
+            "function": 'TIME_SERIES_DAILY',
+            "symbol": stock_name,
+            "apikey": api_key,
+        }
+        self.daily_candles_data:json = None
+
+    def get_data(self) -> json:
+        """request for data to api and return"""
+        with requests.get(self.URL_ALPHAVANTAGE_API, self.URL_ALPHAVANTAGE_API_PARAMS) as res:
+            res.raise_for_status()
+            return res.json()
+
+    def store_to_json_file(self):
+        """store what is currently in 'self.daily_candles_data' to a file"""
+        with open("./Day036-erf/Stock_News/response_data.json", mode='w') as file:
+            json.dump(self.daily_candles_data, file) 
+
+    def read_json_file(self):
+        """read data from resaponse_data.json and return """
+        with open("./Day036-erf/Stock_News/response_data.json", mode='r') as file:
+            return json.load(file)
+
+    def close_price(self,date):
+        """return close price for that stock at a particular date catch with args"""
+        return float(self.daily_candles_data['Time Series (Daily)'][str(date)]['4. close'])  
+
+    def change_percentage(self, newer_date_price:float, earlier_date_price:float) -> float:
+        """calculate and return change percentage between 2 date """
+        return 100 * (newer_date_price/earlier_date_price) - 100
+            

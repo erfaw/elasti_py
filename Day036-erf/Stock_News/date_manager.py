0317027dcc22d93b8b0b772a4d8881e42507a800
erfaw <146765011+erfaw@users.noreply.github.com>
2025-10-21 12:09:09 +0330

    in 'date_manager.py': 1. fill 'today' inside __init__ instead in main, 2. add some type hing

--- Day036-erf/Stock_News/date_manager.py
+++ Day036-erf/Stock_News/date_manager.py
@@ -1,18 +1,21 @@
 import datetime
 class DateManager:
     def __init__(self):
-        """"""
-        self.today = None
+        """hold date details"""
+        self.today = self.current_date()
         self.yesterday = None
         self.before_yesterday = None
 
-    def current_date(self):
+    def current_date(self) -> datetime.date:
+        """return dt.date.today()"""
         return datetime.date.today()
     
     def yesterday_date(self):
+        """return yesterday based on 'self.today' date"""
         return self.today - datetime.timedelta(days=1)
     
     def before_yesterday_date(self):
+        """return day before yesterday based on 'self.today' date"""        
         return self.today - datetime.timedelta(days=2)
     
 

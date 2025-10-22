f1c3abb7918ac218ae0976fd6b3adf7e7cce1522
erfaw <146765011+erfaw@users.noreply.github.com>
2025-10-22 12:43:38 +0330

    till Day037 004 00:01:27 (UNCOMPLETE)

--- Day037-erf/Habit_Tracker/main.py
+++ Day037-erf/Habit_Tracker/main.py
@@ -5,3 +5,18 @@ USERNAME = "erfawnhy"
 TOKEN = os.environ.get("pixela_token")
 PIXELA_ENDPOINT = "https://pixe.la/v1/users/"
 #Build a graph in api server
+header = {
+    "X-USER-TOKEN": TOKEN
+}
+pixela_params = {
+    "id": "erfimerfi1",
+    "name": "someTest",
+    "unit": "death", 
+    "type": "float",
+    "color": "sora",
+}
+with requests.post(
+    url= f"{PIXELA_ENDPOINT}/{USERNAME}/graphs",
+    json= pixela_params,
+    headers=header) as rs:
+    print(rs.text)

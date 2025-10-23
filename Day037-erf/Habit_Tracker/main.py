bc3f4343ef4d2eb599caab2063ecf0940fdf9afb
erfaw <146765011+erfaw@users.noreply.github.com>
2025-10-23 12:49:42 +0330

    in 'main.py': 1. add 'PIXELA-ENDPOINT_USER', 2. ADD 'GRAPH_ID' seperately, 3. add a timedelta to 'current_day' to test, it could be 0 and non effect, 4. add POST A PIXEL TO GRAPH section and made a right request to fill the graph,

--- Day037-erf/Habit_Tracker/main.py
+++ Day037-erf/Habit_Tracker/main.py
@@ -1,17 +1,19 @@
 import subprocess as sp; sp.call('cls', shell=True)
-import requests, os
+import requests, os, datetime as dt
 
 USERNAME = "erfawnhy"
 TOKEN = os.environ.get("pixela_token")
 PIXELA_ENDPOINT = "https://pixe.la/v1/users"
+PIXELA_ENDPOINT_USER = f"{PIXELA_ENDPOINT}/{USERNAME}"
+GRAPH_ID = "erfimerfi1"
+current_day :str = (dt.date.today() - dt.timedelta(days=6) ).strftime(r"%Y%m%d")
 #Build a graph in api server
 header = {
     "X-USER-TOKEN": TOKEN
 }
-
 # ##POST REQUEST TO MAKE A GRAPH
         # pixela_make_graph_params = {
-        #     "id": "erfimerfi1",
+        #     "id": GRAPH_ID,
         #     "name": "someTest",
         #     "unit": "death", 
         #     "type": "float",
@@ -23,4 +25,16 @@ header = {
         #     headers=header) as rs:
         #     print(rs.text)
 
+##POST A PIXEL TO GRAPH
+pixela_post_pixel_params = {
+    "date": current_day,
+    "quantity": "10",
+    # "optionalData": "this is a test optional data on someTest graph"
+}
+with requests.post(
+    url= f"{PIXELA_ENDPOINT_USER}/graphs/{GRAPH_ID}",
+    headers=header,
+    json= pixela_post_pixel_params,
+    ) as res:
+    print(res.text)
 

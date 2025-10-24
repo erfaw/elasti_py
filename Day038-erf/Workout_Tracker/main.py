f680e9aabb967bbc596216224ee4db86b893a502
erfaw <146765011+erfaw@users.noreply.github.com>
2025-10-24 13:55:57 +0330

    file 'nothing.py' renamed to 'main.py' and moved to folder 'Workout_Tracker'

--- Day038-erf/Workout_Tracker/main.py
+++ Day038-erf/Workout_Tracker/main.py
@@ -2,5 +2,7 @@ import os
 import subprocess as sp; sp.call('cls', shell=True)
 NUTRITIONIX_APP_ID = os.environ.get("nutritionix_app_id")
 NUTRITIONIX_APP_KEY = os.environ.get("nutritionix_app_key")
+NUTRITIONIX_ENDPOINT = "https://www.nutritionix.com/business/api"
+NUTRITIONIX_NLP = "v2/natural/exercise"
 print(NUTRITIONIX_APP_ID)
 print(NUTRITIONIX_APP_KEY)

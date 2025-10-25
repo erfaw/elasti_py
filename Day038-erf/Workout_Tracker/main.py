fadeb18895563e2f879d8c45b85588bf4911a77e
erfaw <146765011+erfaw@users.noreply.github.com>
2025-10-25 12:06:18 +0330

    sort of end Day038

--- Day038-erf/Workout_Tracker/main.py
+++ Day038-erf/Workout_Tracker/main.py
@@ -1,8 +1,26 @@
-import os
-import subprocess as sp; sp.call('cls', shell=True)
-NUTRITIONIX_APP_ID = os.environ.get("nutritionix_app_id")
-NUTRITIONIX_APP_KEY = os.environ.get("nutritionix_app_key")
-NUTRITIONIX_ENDPOINT = "https://www.nutritionix.com/business/api"
-NUTRITIONIX_NLP = "v2/natural/exercise"
-print(NUTRITIONIX_APP_ID)
-print(NUTRITIONIX_APP_KEY)
+#FAILED!!!
+        # import os
+        # import subprocess as sp; sp.call('cls', shell=True)
+        # import requests
+        # NUTRITIONIX_APP_ID = os.environ.get("nutritionix_app_id")
+        # NUTRITIONIX_APP_KEY = os.environ.get("nutritionix_app_key")
+        # NUTRITIONIX_ENDPOINT = "https://trackapi.nutritionix.com"
+        # NLP_EXERCISE = "v2/natural/exercise"
+        # query_input = input("Tell me which exercises you did:\n\t")
+        # header = {
+        #     "x-app-id": NUTRITIONIX_APP_ID,
+        #     "x-app-key": NUTRITIONIX_APP_KEY,
+        # }
+        # exercise_params = {
+        #     "query": query_input,
+        #     "weight_kg": 104,
+        #     "height_cm": 188.5,
+        #     "age": 24,
+        # }
+
+        # with requests.post(
+        #         url= f"{NUTRITIONIX_ENDPOINT}/{NLP_EXERCISE}",
+        #         headers= header,
+        #         json= exercise_params
+        #     ) as res:
+        #         print(res.text)

0daa3f1b4e0998e501e7adfa4f2b588e10e985f1
erfaw <146765011+erfaw@users.noreply.github.com>
2025-10-25 14:24:24 +0330

    in 'main.py': 1. 'DataManager()' class imported, 2. 'sheet' object maded from 'DataManager()' , 3. 'sheet.data' printed out,

--- Day039-erf/Cheap_Flight_Deals/main.py
+++ Day039-erf/Cheap_Flight_Deals/main.py
@@ -1,1 +1,6 @@
+import subprocess as sp; sp.call('cls', shell=True)
+from data_manager import DataManager
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+
+sheet = DataManager()
+print(sheet.data)

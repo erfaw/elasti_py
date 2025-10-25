73d78878661d42256e07864b03e031d7dc171835
erfaw <146765011+erfaw@users.noreply.github.com>
2025-10-25 14:23:07 +0330

    in 'data_manager.py' : 1. att 'SHEETY' declared and filled with sheety api details, 2. method 'get_data()' decleared and filled, 3. att 'data' filled with what returns 'get_data()'

--- Day039-erf/Cheap_Flight_Deals/data_manager.py
+++ Day039-erf/Cheap_Flight_Deals/data_manager.py
@@ -1,3 +1,23 @@
+import subprocess as sp; sp.call('cls', shell=True)
+import requests, os
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self):
+        """This class is responsible for talking to the Google Sheet."""
+        self.SHEETY:dict = {
+            "ENDPOINT": "https://api.sheety.co/54bdfdd6059a5dd8664f1a638dbf3770",
+            "PROJECT": "flightDeals",
+            "SHEET": "prices",
+            "HEADERS": {
+                "Authorization": os.environ.get("sheety_api_token")
+            }
+        }
+        self.data:dict = self.get_data()
+
+    def get_data(self) -> dict:
+        """get data from sheet file and return (dict)"""
+        with requests.get(
+        url= f"{self.SHEETY["ENDPOINT"]}/{self.SHEETY["PROJECT"]}/{self.SHEETY["SHEET"]}",
+        headers= self.SHEETY["HEADERS"]
+            ) as response:
+                return response.json()
+                

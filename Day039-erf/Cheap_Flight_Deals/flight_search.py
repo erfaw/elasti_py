8454bbdbd9baf1be6a344f227b49f5c7d480c5db
erfaw <146765011+erfaw@users.noreply.github.com>
2025-10-26 10:35:16 +0330

    in 'flight_search.py': 1. url added , 2. api key gotten from EV, 3. api secret gotten from EV

--- Day039-erf/Cheap_Flight_Deals/flight_search.py
+++ Day039-erf/Cheap_Flight_Deals/flight_search.py
@@ -1,3 +1,7 @@
+import os
 class FlightSearch:
-    #This class is responsible for talking to the Flight Search API.
-    pass
+    def __init__(self):
+        """This class is responsible for talking to the Flight Search API."""
+        self.URL_AMADEUS = "test.api.amadeus.com"
+        self.API_KEY_AMADEUS = os.environ.get("API_KEY_AMADEUS")
+        self.API_SECRET_AMADEUS = os.environ.get("API_SECRET_AMADEUS")

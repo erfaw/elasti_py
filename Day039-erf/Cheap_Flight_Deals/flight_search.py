2aa2962b18271a59ae531c00aaa378f97ee56de7
erfaw <146765011+erfaw@users.noreply.github.com>
2025-10-26 11:45:36 +0330

    in 'flight_search.py': 1. fix 'URL_AMADEUS', 2. add 'ACCESS_TOKEN_AMADEUS' from EV, 3. declare method 'get_access_token()' to POST request for get access token from api (not work with Iran IP, we done it with pythonanywhere.com bash console :( )

--- Day039-erf/Cheap_Flight_Deals/flight_search.py
+++ Day039-erf/Cheap_Flight_Deals/flight_search.py
@@ -1,7 +1,28 @@
-import os
+import os, requests
 class FlightSearch:
     def __init__(self):
         """This class is responsible for talking to the Flight Search API."""
-        self.URL_AMADEUS = "test.api.amadeus.com"
+        self.URL_AMADEUS = "https://test.api.amadeus.com"
         self.API_KEY_AMADEUS = os.environ.get("API_KEY_AMADEUS")
         self.API_SECRET_AMADEUS = os.environ.get("API_SECRET_AMADEUS")
+        self.ACCESS_TOKEN_AMADEUS = os.environ.get("ACCESS_TOKEN_AMADEUS")
+
+    def get_access_token(self):
+        """NOTICE: first try to catch it from EV: make POST request to get access token from site"""
+        headers = {
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+        body_request = {
+            "grant_type": "client_credentials",
+            "client_id": self.API_KEY_AMADEUS,
+            "client_secret": self.API_SECRET_AMADEUS
+        }
+        with requests.post(
+            url= f"{self.URL_AMADEUS}/v1/security/oauth2/token",
+            data= body_request,
+            headers= headers
+        ) as res:
+            res.raise_for_status()
+            return res.json()
+            # return res
+        

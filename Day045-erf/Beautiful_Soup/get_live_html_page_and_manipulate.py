8a5eeda639c3c3541c51080e99777086078e5fc8
erfaw <146765011+erfaw@users.noreply.github.com>
2025-11-01 12:24:56 +0330

    1.successfuly got title of first article and print it on console

--- Day045-erf/Beautiful_Soup/get_live_html_page_and_manipulate.py
+++ Day045-erf/Beautiful_Soup/get_live_html_page_and_manipulate.py
@@ -5,4 +5,8 @@ with requests.get(url="https://news.ycombinator.com/") as response:
     response.raise_for_status()
     web_page = BeautifulSoup(response.text, 'html.parser')
 
-print(web_page.title.string)
+##GET TITLE OF FIRST ARTICLE ON HACKERNEWS
+first_article_title = web_page.select_one(
+    selector=".titleline a"
+)
+print(first_article_title.string)

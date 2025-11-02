cf59650896f913657b82b4d46c3533fdfd3e1e3c
erfaw <146765011+erfaw@users.noreply.github.com>
2025-11-02 11:05:03 +0330

    add some comments

--- Day045-erf/Beautiful_Soup/get_live_html_page_and_manipulate.py
+++ Day045-erf/Beautiful_Soup/get_live_html_page_and_manipulate.py
@@ -10,10 +10,11 @@ all_tr = web_page.select(
     selector="#bigbox table tr"
 )[:-2] #except last 2 , they wasn't article
 articles = []
-#slice 3 by 3 
+##SLICE 3 BY 3 (EACH 3 <tr> TAG CONSIDER AS ONE ARTICLE (INCLUDE TITLE, LINK, UPVOTE AND ETC)) 
 for i in range(0, len(all_tr), 3):
     articles.append(all_tr[i:i+3])
 
+##LOOP THROUGH AND CATCH DATA WHICH WE WANT
 article_title = []
 article_link = []
 article_upvote = []
@@ -28,9 +29,13 @@ data_to_pass = {
     "article_link" : article_link,
     "article_upvote" : article_upvote
 }
+
+##MAKE PANDAS.DATAFRAME FOR ARTICLES
 articles_data = pandas.DataFrame(data_to_pass,None, ["article_title", "article_link", "article_upvote"], None, True)
-# articles_data.to_excel(excel_writer= "./Day045-erf/Beautiful_Soup/articles_data.xlsx",sheet_name='articles data', index=False)
+articles_data.sort_values("article_upvote", ascending=0, ignore_index=True, inplace=True,)
+print(articles_data)
 
+##CATCH THE MAXIMUM VALUE OF "article_upvote" COLUMN
 highest_upvote_article = articles_data[articles_data["article_upvote"] == articles_data["article_upvote"].max()]
-
+# articles_data.to_excel(excel_writer= "./Day045-erf/Beautiful_Soup/articles_data.xlsx",sheet_name='articles data', index=False)
 print(highest_upvote_article.get("article_link").item())

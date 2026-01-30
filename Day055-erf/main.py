dda0c53e650932f9f734b41b95bf197782c477b4
erfaw <erfawn.h@gmail.com>
2026-01-30 11:26:50 +0330

    in 'Day055-erf\main.py': 1. add 3 decorator to make it bold and em and underline, 2. add '/gorba' route with a cat image, 3. add a 'bye()' function and a '/bye' route with using decorators on it,

--- Day055-erf/main.py
+++ Day055-erf/main.py
@@ -1,6 +1,24 @@
 from flask import Flask
 app = Flask(__name__)
 
+# Decorator: add bold element
+def make_bold(f):
+    def wrap_f():
+        return '<b>'+ f() + '<b/>'
+    return wrap_f
+
+# Decorator: add emphasis element
+def make_emphasis(f):
+    def wrap_f():
+        return '<em>'+ f() + '<em/>'
+    return wrap_f
+
+# Decorator: add underline element
+def make_underline(f):
+    def wrap_f():
+        return '<u>' + f() + '<u/>'
+    return wrap_f
+
 @app.route('/')
 def hello_world():
     return '<a style="color:red; border: 1px solid white; font-size: 50px;">Hello, World!<a/>'
@@ -8,6 +26,17 @@ def hello_world():
 @app.route('/<user_name>')
 def greet(user_name):
     return f"salam {user_name} kesafat :) 😂"
+
+@app.route('/gorba')
+def kitty_page():
+    return f'<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHVnw4nOFCGn_AR91QwGxE8vQEimLGNkopgA&s"/>'
+
+@app.route('/bye')
+@make_bold
+@make_emphasis
+@make_underline
+def bye():
+    return 'Bye'
 
 if __name__ == "__main__":
     app.run(debug= True)

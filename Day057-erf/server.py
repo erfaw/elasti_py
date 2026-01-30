b96563cf9b5fafc6620a1b2e4a845747e877ba58
erfaw <erfawn.h@gmail.com>
2026-01-30 22:04:49 +0330

    in 'Day057-erf\server.py': 1. import requests, 2. add commented codes for 'API call for gender' (we got connectoin abortions), need fix

--- Day057-erf/server.py
+++ Day057-erf/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from datetime import datetime as dt
+import requests as rq
 
 # Get this year
 year = dt.now().date().year
@@ -9,6 +10,18 @@ app = Flask(__name__)
 @app.route('/')
 def home_page():
     return render_template("index.html", year=year,)
+
+@app.route('/guess/<name>')
+def guess(name):
+    # # API call for gender
+    # gender_api_link = "https://api.genderize.io/"
+    # params = {"name": name}
+    # with rq.get(gender_api_link, params=params,) as res:
+    #     res.raise_for_status()
+    #     content = res.json()
+    # print(content)
+
+    return render_template("index.html", year=year, name=name, )
 
 if __name__ == "__main__":
     app.run(debug=True)

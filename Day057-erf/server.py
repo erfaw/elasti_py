593b846c082e0d3fc29255649f8741f5cb7383d4
erfaw <erfawn.h@gmail.com>
2026-01-31 13:52:02 +0330

    in 'Day057-erf\server.py': 1. in 'guess()['/guess/<name>/']: done 2 api call for age and gender then render template, also add try-except to catch RequestException, NOTICE: with iran normal network we get 'Connection Aborted' but its good with VPN

--- Day057-erf/server.py
+++ Day057-erf/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from datetime import datetime as dt
 import requests as rq
+from requests import HTTPError
 
 # Get this year
 year = dt.now().date().year
@@ -13,15 +14,29 @@ def home_page():
 
 @app.route('/guess/<name>')
 def guess(name):
-    # # API call for gender
-    # gender_api_link = "https://api.genderize.io/"
-    # params = {"name": name}
-    # with rq.get(gender_api_link, params=params,) as res:
-    #     res.raise_for_status()
-    #     content = res.json()
-    # print(content)
+    params = {"name": name, "country_id": "IR"}
+    READ_TIMEOUT = 5
+    # API call for gender
+    gender_api_link = f"https://api.genderize.io"
+    try: 
+        res = rq.get(gender_api_link, params,timeout= READ_TIMEOUT)
+        res.raise_for_status()
+        gender = res.json()['gender']
+    except rq.exceptions.RequestException as e:
+        gender = 'Error!'
+        print(e)
+    
+    # API call for age
+    age_api_link = f"https://api.agify.io"
+    try:
+        res = rq.get(age_api_link, params, timeout= READ_TIMEOUT)
+        res.raise_for_status()
+        age = res.json()['age']
+    except rq.exceptions.RequestException as e:
+        age = 'Error!'
+        print(e)
 
-    return render_template("index.html", year=year, name=name, )
+    return render_template("guess.html", year=year, name=name, gender=gender, age=age)
 
 if __name__ == "__main__":
     app.run(debug=True)

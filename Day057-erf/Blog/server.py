101d49b82f61731fc63e1abcf7c572939cb726c1
erfaw <erfawn.h@gmail.com>
2026-01-31 15:16:59 +0330

    in 'Day057-erf\Blog\server.py': 1. add 'root_url' for testing its easier, 2. in 'blogs()': send 'root_url, blogs_url' to 'all_blogs.html', 3. make a new route: 'blog_post()' with route => f'/{blogs_url}/<int:post_id>', 4. in 'blog_post()': render from 'single_blog.html' and send 'all_blogs' and 'post_id'(from url) to it, RUN-CHECK: GOOD, but without any styiling

--- Day057-erf/Blog/server.py
+++ Day057-erf/Blog/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import requests as rq
+root_url = "http://127.0.0.1:5000"
 
 # blogs api
 all_blogs_link = "https://api.npoint.io/4ef3129582a92084b09d"
@@ -15,11 +16,16 @@ app = Flask(__name__)
 def home_page():
     return render_template('home_page.html', blogs_url=blogs_url)
 
-# Build blogs page
+# Build all blogs page
 blogs_url = r"blogs"
 @app.route(f'/{blogs_url}')
 def blogs():
-    return render_template("blog_template.html", all_blogs=all_blogs)
+    return render_template("all_blogs.html", all_blogs=all_blogs, root_url=root_url, blogs_url=blogs_url)
+
+# Build each blog post page
+@app.route(f'/{blogs_url}/<int:post_id>')
+def blog_post(post_id):
+    return render_template("single_blog.html", all_blogs=all_blogs, post_id=post_id)
 
 # Run server
 if __name__ == "__main__":

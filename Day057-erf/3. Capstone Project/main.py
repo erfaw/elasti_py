405289bda630ef81212f81523d165363d0a20d59
erfaw <erfawn.h@gmail.com>
2026-01-31 16:07:30 +0330

    in 'Day057-erf\3. Capstone Project\main.py': 1. in 'home()': get all blogs from 'all_blogs_link' and store it in 'all_blogs' variable, then send it to 'index.html' to render, 2. also import requests,

--- Day057-erf/3. Capstone Project/main.py
+++ Day057-erf/3. Capstone Project/main.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
+import requests as rq
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    # Blogs api
+    all_blogs_link = "https://api.npoint.io/4ef3129582a92084b09d"
+    # Get all blog posts
+    all_blogs = rq.get(all_blogs_link).json()
+    return render_template("index.html", all_blogs=all_blogs)
 
 if __name__ == "__main__":
     app.run(debug=True)

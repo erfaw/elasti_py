c748bdddacfc8ac851cb141550dbe9111e63a174
erfaw <erfawn.h@gmail.com>
2026-01-31 14:53:48 +0330

    in 'Day057-erf\Blog\server.py': 1. add sections 'blogs api' and 'Get all blog posts', 2. in 'home_page()': return 'home_page.html' with blogs url, 3. in 'blogs()': return 'blog_template.html' with 'all_blogs', RUN-CHECK: Good,

--- Day057-erf/Blog/server.py
+++ Day057-erf/Blog/server.py
@@ -1,13 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
+import requests as rq
+
+# blogs api
+all_blogs_link = "https://api.npoint.io/4ef3129582a92084b09d"
+
+# Get all blog posts
+all_blogs = rq.get(all_blogs_link).json()
 
 # Build Flask app
 app = Flask(__name__)
 
 # Build root page
 @app.route('/')
-def home():
-    pass
+def home_page():
+    return render_template('home_page.html', blogs_url=blogs_url)
+
+# Build blogs page
+blogs_url = r"blogs"
+@app.route(f'/{blogs_url}')
+def blogs():
+    return render_template("blog_template.html", all_blogs=all_blogs)
 
 # Run server
-if __name__ == "__name__":
+if __name__ == "__main__":
     app.run(debug= True)

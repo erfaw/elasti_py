96b22ad6756a8a67fece0b70463117c3f5276019
erfaw <erfawn.h@gmail.com>
2026-01-31 16:32:25 +0330

    in 'Day057-erf\3. Capstone Project\templates\index.html': 1. in <body><div.wrapper><div.content><a>: fix syntax for 'url_for()',
    in 'Day057-erf\3. Capstone Project\templates\post.html':
    1. build page with 'blog_to_render' gotten variable,
    
    in 'Day057-erf\3. Capstone Project\main.py':
    1. move out 'Get all blog posts' and 'Blogs api' from 'home()',
    2. add explanation comments,
    3. in 'Build a post page for a blog id' in 'post()': add section 'Gather specific blog post before rendering' and render with that,
    RUN-CHECK: GOOD,

--- Day057-erf/3. Capstone Project/main.py
+++ Day057-erf/3. Capstone Project/main.py
@@ -1,16 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 import requests as rq
 
+# Blogs api
+all_blogs_link = "https://api.npoint.io/4ef3129582a92084b09d"
+# Get all blog posts
+all_blogs = rq.get(all_blogs_link).json()
 
+# Build Flask app
 app = Flask(__name__)
 
+# Build root page
 @app.route('/')
 def home():
-    # Blogs api
-    all_blogs_link = "https://api.npoint.io/4ef3129582a92084b09d"
-    # Get all blog posts
-    all_blogs = rq.get(all_blogs_link).json()
     return render_template("index.html", all_blogs=all_blogs)
+
+# Build a post page for a blog id
+@app.route('/post/<int:blog_id>')
+def post(blog_id):
+
+    # Gather specific blog post before rendering
+    for blog in all_blogs:
+        if blog_id == blog['id']:
+            blog_to_render = blog
+
+    return render_template(
+        "post.html",
+        # Send specific blog to render
+        blog_to_render=blog_to_render,
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)

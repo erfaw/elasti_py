f13de976b90d65ba59479317191e1cb47b7f5f00
erfaw <erfawn.h@gmail.com>
2026-02-02 18:32:08 +0330

    in 'Day059-erf\Upgraded Blog\main.py': 1. add sections 'API for blogs' 'Get all blog posts', 2. send 'all_blogs' revieved from api to 'home_page()'

--- Day059-erf/Upgraded Blog/main.py
+++ Day059-erf/Upgraded Blog/main.py
@@ -1,4 +1,10 @@
 from flask import Flask, render_template
+import requests as rq
+
+# API for blogs
+all_blogs_link = "https://api.npoint.io/4ef3129582a92084b09d"
+# Get all blog posts
+all_blogs = rq.get(all_blogs_link).json()
 
 # Build flask app
 app = Flask(__name__)
@@ -8,6 +14,7 @@ app = Flask(__name__)
 def home_page():
     return render_template(
         "index.html",
+        all_blogs=all_blogs
     )
 
 # About page

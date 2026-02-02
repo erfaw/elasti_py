a7712a7d2e7f1d65fc6a312b678ef955a75d1f0d
erfaw <erfawn.h@gmail.com>
2026-02-02 18:01:03 +0330

    in 'Day059-erf\Upgraded Blog\templates\index.html': 1. in <body> in '<!-- Navigation-->': fix all 'href' for each link to the other pages in navigation bar,
    in 'Day059-erf\Upgraded Blog\main.py':
    1. add section 'About page' and 'Contact page': render their own page

--- Day059-erf/Upgraded Blog/main.py
+++ Day059-erf/Upgraded Blog/main.py
@@ -10,6 +10,20 @@ def home_page():
         "index.html",
     )
 
+# About page
+@app.route('/about')
+def about_page():
+    return render_template(
+        "about.html"
+    )
+
+# Contact page
+@app.route('/home')
+def contact_page():
+    return render_template(
+        "contact.html"
+    )
+
 # Idiom run
 if __name__ == "__main__":
     app.run(debug= True)

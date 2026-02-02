d54c847a232a0df87926f9622522a1a306be51bd
erfaw <erfawn.h@gmail.com>
2026-02-02 18:48:23 +0330

    in 'Day059-erf\Upgraded Blog\main.py': 1. open json file and read instead of api call (codes about api call commented) because of connection aborted NOTICE: need to catch and render in 'index.html'

--- Day059-erf/Upgraded Blog/main.py
+++ Day059-erf/Upgraded Blog/main.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
 import requests as rq
+import json
 
 # API for blogs
 all_blogs_link = "https://api.npoint.io/4ef3129582a92084b09d"
 # Get all blog posts
-all_blogs = rq.get(all_blogs_link).json()
+# all_blogs = rq.get(all_blogs_link, timeout=5).json()
+with open(r"G:\myDocuments\Programming\Python\elasti_py\Day059-erf\Upgraded Blog\static\blog-data.txt") as file:
+    all_blogs = json.load(file)
 
 # Build flask app
 app = Flask(__name__)

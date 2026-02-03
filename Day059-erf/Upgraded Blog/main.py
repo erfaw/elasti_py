c77ff80fce8f537ad5f65936de68aa7e08a45644
erfaw <erfawn.h@gmail.com>
2026-02-03 10:02:30 +0330

    in 'Day059-erf\Upgraded Blog\main.py': 1. import pathlib.Path, 2. change a explanasion comment for API call, 3. add section 'Get root folder, static and templates address', 4. in section 'Get root folder, static and templates address': using pathlib.Path set 'root_path' 'static_path' 'templates_path' variables, 5. add explanation comment to with open json file, 'Read blog posts from JSON file instead of API call', 6. in 'Read blog posts from JSON file instead of API call': replace root path of blog-data.txt with 'root_path'

--- Day059-erf/Upgraded Blog/main.py
+++ Day059-erf/Upgraded Blog/main.py
@@ -1,12 +1,20 @@
 from flask import Flask, render_template
 import requests as rq
 import json
+from pathlib import Path
 
 # API for blogs
 all_blogs_link = "https://api.npoint.io/4ef3129582a92084b09d"
-# Get all blog posts
+# # Get all blog posts with api call => (commented because of Connection Aborted)
 # all_blogs = rq.get(all_blogs_link, timeout=5).json()
-with open(r"G:\myDocuments\Programming\Python\elasti_py\Day059-erf\Upgraded Blog\static\blog-data.txt") as file:
+
+# Get root folder, static and templates address
+root_path = Path(__file__).parent
+static_path = root_path/'static'
+templates_path = root_path/'templates' 
+
+# Read blog posts from JSON file instead of API call
+with open(fr"{root_path}\static\blog-data.txt") as file:
     all_blogs = json.load(file)
 
 # Build flask app

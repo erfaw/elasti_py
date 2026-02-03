5c1f2c39f7306c9ba82a618732e3d70c06399685
erfaw <erfawn.h@gmail.com>
2026-02-03 19:29:40 +0330

    in 'Day060-erf\1. learn about POST and Form tag': 1. in 'index.html': in <form>: add attributes 'method='post'' and action=url_for() , in <input> s: add name attribute 2. in 'main.py': import flask.request, add a new route '/login' to using methods='post' to get data from user <form> and use in python

--- Day060-erf/1. learn about POST and Form tag/main.py
+++ Day060-erf/1. learn about POST and Form tag/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask import request 
 
 # Build flask app
 app = Flask(__name__)
@@ -9,6 +10,17 @@ def home_page():
     return render_template(
         "index.html"
     )
+
+@app.route('/login', methods=["POST"])
+def recieve_data():
+    if request.method == 'POST':
+        first_name = request.form['firstName']
+        password = request.form['password']
+    return f"""
+<h1>
+    first name: {first_name}, password: {password}
+</h1>
+"""
 
 # Run idiom
 if __name__ == "__main__":

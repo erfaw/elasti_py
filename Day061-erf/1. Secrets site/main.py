746bc7c7e08be465369b0e48ccad02575f9af6e8
erfaw <erfawn.h@gmail.com>
2026-02-04 15:06:42 +0330

    in 'Day061-erf\1. Secrets site\main.py': 1. in route('/login', get,post): add explanation comments, build work flow rightfully to access administrator page,

--- Day061-erf/1. Secrets site/main.py
+++ Day061-erf/1. Secrets site/main.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
 from wtforms import EmailField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
 
+# Build class for login form
 class LoginForm(FlaskForm):
     email = EmailField(label='Email', validators=[DataRequired(), Email()])
     password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8)])
@@ -17,12 +18,32 @@ def home():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    # Build a login form obj
     login_form = LoginForm()
-    login_form.validate_on_submit()
-    return render_template(
-        "login.html",
-        form= login_form
-    )
+    # If 'POST'
+    if request.method == 'POST':
+        # Check if validation was alright
+        if login_form.validate_on_submit():
+            # Check for adminstrator access
+            if login_form.email.data == 'admin@email.com' and login_form.password.data == '12345678':
+                # Return admin page
+                return render_template("success.html")
+            else:
+                # Otherwise return Denied page
+                return render_template("denied.html")
+        # Must be some issue with entry values
+        else:
+            # Return normal page with errors
+            return render_template(
+            "login.html",
+            form= login_form
+            )
+    elif request.method == 'GET':
+        # If 'GET': render normal page
+        return render_template(
+            "login.html",
+            form= login_form
+        )
 
 
 if __name__ == '__main__':

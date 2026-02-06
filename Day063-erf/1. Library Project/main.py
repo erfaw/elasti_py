82961f4cfa0f1e395b205c9a8097e62e80f09e09
erfaw <erfawn.h@gmail.com>
2026-02-06 21:50:55 +0330

    in 'Day063-erf\1. Library Project\main.py': 1. build Base() class, 2. build db variable, 3. make 'db_path' to work with our file structures, 4. fill app config db URI using 'db_path', 5. db init with app, 6. build Books() model, 7. done create all on db, 8. in 'add()' : make new book obj and add and commit to db, with opening app_context,

--- Day063-erf/1. Library Project/main.py
+++ Day063-erf/1. Library Project/main.py
@@ -1,9 +1,33 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import String
+from pathlib import Path
+
+class Base(DeclarativeBase): pass
+
+db = SQLAlchemy(model_class= Base)
 
 app = Flask(__name__)
 
-all_books = []
+ROOT_DIR = Path(__file__).resolve().parent
+(ROOT_DIR / "instance").mkdir(exist_ok=True)
+db_path = (ROOT_DIR/"instance"/"library_books.db").as_posix()
 
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_path}"
+db.init_app(app)
+
+class Books(Base):
+    __tablename__=r"books"
+    id: Mapped[int] = mapped_column(primary_key= True)
+    name: Mapped[str] = mapped_column(String(250),nullable= False, unique=True)
+    author: Mapped[str] = mapped_column(String(250), nullable= False)
+    rate: Mapped[float] = mapped_column(nullable=False)
+
+with app.app_context():
+    db.create_all()
+
+all_books = []
 
 @app.route('/')
 def home():
@@ -22,6 +46,11 @@ def add():
             "book_rate" : request.form.get('book_rate'),
         }
         all_books.append(book_record)
+
+        book_obj = Books(name=book_record['book_name'], author=book_record['book_author'], rate= book_record['book_rate'])
+        with app.app_context():
+            db.session.add(book_obj)
+            db.session.commit()
 
         return redirect(
                 url_for('home', is_submited=True)

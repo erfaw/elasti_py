00168b5caaab7301afbb2774c4dec5d8fc440fe1
erfaw <erfawn.h@gmail.com>
2026-02-10 02:01:51 +0330

    in 'Day066-erf\1. introdoce to RESTful APIs\main.py': 1. make 'ROOT_DIR', 2. do .mkdir() on 'instance' folder , 3. initialize 'db_path', 4. for sure: use .as_posix() on 'db_path', 5. import pathlib.Path, 6. make 'app.config['SQLALCHEMY_DATABASE_URI']' initilization a f-string and fetch 'db_path', 7. remove additional comments and docstring

--- Day066-erf/1. introdoce to RESTful APIs/main.py
+++ Day066-erf/1. introdoce to RESTful APIs/main.py
@@ -2,32 +2,21 @@ from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Boolean
+from pathlib import Path
 
-'''
-Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
+ROOT_DIR = Path(__file__).resolve().parent
+(ROOT_DIR / "instance").mkdir(exist_ok= True)
+db_path = ROOT_DIR / 'instance' / 'cafes.db'
+db_path.as_posix()
 
 app = Flask(__name__)
 
-# CREATE DB
-class Base(DeclarativeBase):
-    pass
-# Connect to Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
+class Base(DeclarativeBase): pass
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
-
-# Cafe TABLE Configuration
 class Cafe(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
@@ -41,15 +30,12 @@ class Cafe(db.Model):
     can_take_calls: Mapped[bool] = mapped_column(Boolean, nullable=False)
     coffee_price: Mapped[str] = mapped_column(String(250), nullable=True)
 
-
 with app.app_context():
     db.create_all()
-
 
 @app.route("/")
 def home():
     return render_template("index.html")
-
 
 # HTTP GET - Read Record
 
@@ -58,7 +44,6 @@ def home():
 # HTTP PUT/PATCH - Update Record
 
 # HTTP DELETE - Delete Record
-
 
 if __name__ == '__main__':
     app.run(debug=True)

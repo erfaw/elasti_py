a48dbf8829acfe1d3f09952a0890f429323b90dc
erfaw <erfawn.h@gmail.com>
2026-02-11 01:25:41 +0330

    in 'Day067-erf\Upgraded Upgraded Blog Site\main.py': 1. declare 'delete_post(post_id) on route '/delete/<int:post_id>' just GET, 2. in 'delete_post()':     1) fetch 'post_to_delete',     2) delete and commit to db,     3) redirect to 'get_all_posts()' '/' route, with 'is_deleted=True' passed,

--- Day067-erf/Upgraded Upgraded Blog Site/main.py
+++ Day067-erf/Upgraded Upgraded Blog Site/main.py
@@ -118,9 +118,15 @@ def edit_post(post_id):
         is_edit = True,
     )
 
-# TODO: delete_post() to remove a blog post from the database
+@app.route('/delete/<int:post_id>')
+def delete_post(post_id):
+    post_to_delete = db.get_or_404(BlogPost, post_id)
+    db.session.delete(post_to_delete)
+    db.session.commit()
+    return redirect(
+        url_for('get_all_posts', is_deleted = True)
+    )
 
-# Below is the code from previous lessons. No changes needed.
 @app.route("/about")
 def about():
     return render_template("about.html")

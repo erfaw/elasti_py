d880619b5b4994d2d1042bfbeb8aa3e21875ce48
erfaw <erfawn.h@gmail.com>
2026-02-16 00:00:47 +0330

    in 'Day069-erf\Upgraded Upgraded Upgraded Blog site\main.py': 1. comment some codes to just first run

--- Day069-erf/Upgraded Upgraded Upgraded Blog site/main.py
+++ Day069-erf/Upgraded Upgraded Upgraded Blog site/main.py
@@ -2,7 +2,7 @@ from datetime import date
 from flask import Flask, abort, render_template, redirect, url_for, flash
 from flask_bootstrap import Bootstrap5
 from flask_ckeditor import CKEditor
-from flask_gravatar import Gravatar
+# from flask_gravatar import Gravatar
 from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
@@ -49,8 +49,8 @@ class BlogPost(db.Model):
 
 
 # TODO: Create a User table for all your registered users. 
-class User(db.Model):
-    pass
+# class User(db.Model):
+#     pass
 
 with app.app_context():
     db.create_all()
